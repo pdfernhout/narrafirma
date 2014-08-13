d081138b1c9433d8d88632207bdfc0eae3c67297
Cynthia Kurtz <cfkurtz@kurtz-fernhout.com>
2014-08-13 14:36:43 -0400

    first check-in of helptexts and translations; changing specification of questions and answers and pages

--- narracoach_questions.js
+++ narracoach_questions.js
@@ -1,28 +1,21 @@
 "use strict";
 
-var page_Introduction = {
-    title: "Introduction",
-    pageText:
-"Hello and welcome to NarraCoach. This application will guide you through carrying out a participatory narrative inquiry (PNI) project.\n\
-Your project will go through these steps:<br>\
-<br>\
-* Planning: Tell stories and make sense of your goals and hopes for your project.<br>\
-* Collection: Set up your questions and collect stories in person or over the web.<br>\
-* Catalysis: Prepare some \"food for thought\" for people to use in working with the stories you have collecteed.<br>\
-* Sensemaking: Guide people through working with the stories you have collected.<br>\
-* Intervention: Optionally, design one or more interventions to guide the flow of stories.<br>\
-* Return: Gather information about your project -- impact and reflect on what you have learned."
+// for questions and choices, id field is lookup field for text
+// for pages, two lookups: for title, append "_title"; for text, append "_text"
+// system should tell us if title and text strings are not defined for page
+
+var page_generalIntro = {
+	id: "page_generalIntro",
 };
 
 var page_ProjectStoriesIntro = {
-    title: "Project Stories Introduction",
-    pageText: "The best way to start planning a PNI project is to tell stories about the project. Tell your stories as future histories, as if your project had already taken place.",
+	id: "page_ProjectStoriesIntro",
     // this page will list stories the user has entered
-    // it will also have a button called "tell story" which will invoke the tell project story page
+    // on the story list widget will be an "add story" button (which will not be seen in other uses of the widget)
     questions: [
         {
-            id: "q1", 
-            text: "Please add stories below",
+            id: "widget_storyList", 
+            text: "Please add stories below", // cfk not putting this into translation as it will go away later
             // type: "storyList",
             type: "textarea",
             options: "page_projectStoryEntry",
@@ -31,81 +24,32 @@ var page_ProjectStoriesIntro = {
     
 };
 
-var choice_projectStoryScenarioType_askMeAnything = {
-    value: "askMeAnything", 
-    display: "Ask me anything", 
-    hint: "If you could ask any person any question and would be guaranteed to get an honest answer (magically), whom would you ask, about what, and why?"
-};
-
-
-/*
-         {
-         id: "askMeAnything", 
-         text: "Ask me anything", 
-         hint: "If you could ask any person any question and would be guaranteed to get an honest answer (magically), whom would you ask, about what, and why?"
-         },
-         {
-         id: "magicEars", 
-         text: "Magic ears", 
-         hint: "If you could overhear anyone talking to anyone at any time and in any place, whom would you want to listen to, where and when, and why?"
-         },
-         {
-         id: "flyOnTheWall", 
-         text:"Fly on the wall", 
-         hint:"If you could observe any situation or event, what situation would you want to witness, and why?"
-         },
-         {
-         id: "projectAspects", 
-         text: "Project aspects", 
-         hint: "Consider an important aspect of your project: the people you will be asking to tell stories and/or the topic you will be exploring."
-         },
-         {
-         id:"customQuestion", 
-         text: "Describe a scenario that answers another question.", 
-         hint: "You can make up your own question to answer."
-         }
-*/
+var page_projectStoryEntry = {
+		id: "page_projectStoryEntry",
+	    questions: [
+	        question_projectStoryScenarioType,
+	        question_projectStoryScenarioCustomQuestion
+	    ]
+	};
 
 var question_projectStoryScenarioType = {
-    id: "projectStoryScenarioType",
-    text: "Please choose one of these questions, which you will answer by describing a *scenario*.",
+    id: "question_projectStoryScenarioType",
     type: "select",
-    choices: [
-        choice_projectStoryScenarioType_askMeAnything, 
-        //projectStoryScenarioType_magicEars,
-        //projectStoryScenarioType_flyOnTheWall,
-        //projectStoryScenarioType_projectAspects,
-        //projectStoryScenarioType_customQuestion
-        ],
-    options: "askMeAnything\nmagicEars\nflyOnTheWall\nprojectAspects\ncustomQuestion",
-    help: "\
-Ask me anything: If you could ask any person any question and would be guaranteed to get an honest answer (magically), whom would you ask, about what, and why?<br>\
-Magic ears: If you could overhear anyone talking to anyone at any time and in any place, whom would you want to listen to, where and when, and why?<br>\
-Fly on the wall: If you could observe any situation or event, what situation would you want to witness, and why?<br>\
-Project aspects: Consider an important aspect of your project: the people you will be asking to tell stories and/or the topic you will be exploring.<br>\
-Describe a scenario that answers another question: You can make up your own question to answer.\
-"
+    choices: ["askMeAnything", "magicEars", "flyOnTheWall", "projectAspects", "customQuestion"]
+    help: "projectStoryScenarioType"
 }
 
-function projectStoryScenarioType_customQuestion_enabled() {
+function question_projectStoryScenarioTypeCustomQuestion_enabled() {
     // TO DO
     return true;
 }
 
 var question_projectStoryScenarioCustomQuestion = {
-    id: "projectStoryScenarioCustomQuestion",
-    enabled: projectStoryScenarioType_customQuestion_enabled,
-    text: "Please enter the question to which this project story is an answer.",
+    id: "question_projectStoryScenarioCustomQuestion",
+    enabled: question_projectStoryScenarioTypeCustomQuestion_enabled,
+    text: "question_projectStoryScenarioTypeCustomQuestion",
     type: "textArea",
 }
-
-var page_ProjectStoryEntry = {
-    title: "Project story entry",
-    questions: [
-        question_projectStoryScenarioType,
-        question_projectStoryScenarioCustomQuestion
-    ]
-};
 
 var pageList = [
     page_Introduction,
---
+++ translations.js
@@ -0,0 +1,28 @@
+var translations = {
+		// page: introduction 
+		"page_generalIntro_title": "Introduction",
+		"page_generalIntro_text": "<p>Hello and welcome to NarraCoach.</p>"
+			+ "<p>This application will guide you through carrying out a participatory narrative inquiry (PNI) project.</p>"
+			+ "<p>TYour project will go through these steps:</p>"
+			+ "<ul><li>Planning: Tell stories and make sense of your goals and hopes for your project.</li>"
+			+ "<li>Collection: Set up your questions and collect stories in person or over the web.</li>"
+			+ "<li>Catalysis: Prepare some \"food for thought\" for people to use in working with the stories you have collected.</li>"
+			+ "<li>Sensemaking: Guide people through working with the stories you have collected.</li>"
+			+ "<li>Intervention: Optionally, design one or more interventions to guide the flow of stories.</li>"
+			+ "<li>Return: Gather information about your project -- impact and reflect on what you have learned.",
+		// page: project stories
+		"page_projectStoriesIntro_title": "Project Stories Introduction",
+		"page_projectStoriesIntro_text": "The best way to start planning a PNI project is to tell stories about the project. Tell your stories as future histories, as if your project had already taken place.,
+		// question: project story scenario
+		"question_projectStoryScenarioType": "Please choose one of these questions, which you will answer by describing a <b>scenario</b>.",
+		"question_projectStoryScenarioType_choice_askMeAnything": "Ask me anything",
+		"question_projectStoryScenarioType_choice_magicEars": "Magic ears",
+		"question_projectStoryScenarioType_choice_flyOnTheWall": "Fly on the wall",
+		"question_projectStoryScenarioType_choice_projectAspects": "Project aspects",
+		"question_projectStoryScenarioType_choice_customQuestion": "Custom question",
+		// question: custom scenario question (only asked if they chose custom question option above)
+		"question_projectStoryScenarioTypeCustomQuestion": "Please enter the question to which this project story is an answer.",
+		// widget: story list (user can review stories entered, and (in this case) enter new stories)
+		"widget_storyList": ""
+		
+}
