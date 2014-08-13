cbe89d87b88c4a0074e3b60f89b1ab2f8e2ece89
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-08-13 17:20:53 -0400

    Using translations from translations.js

--- narracoach_questions.js
+++ narracoach_questions.js
@@ -8,34 +8,27 @@ var page_generalIntro = {
 	id: "page_generalIntro",
 };
 
-var page_ProjectStoriesIntro = {
-	id: "page_ProjectStoriesIntro",
+var question_storyList = {
+    id: "widget_storyList", 
+    text: "Please add stories below", // cfk not putting this into translation as it will go away later
+    // type: "storyList",
+    type: "textarea",
+    options: "page_projectStoryEntry",
+};
+
+var page_projectStoriesIntro = {
+    id: "page_projectStoriesIntro",
     // this page will list stories the user has entered
     // on the story list widget will be an "add story" button (which will not be seen in other uses of the widget)
     questions: [
-        {
-            id: "widget_storyList", 
-            text: "Please add stories below", // cfk not putting this into translation as it will go away later
-            // type: "storyList",
-            type: "textarea",
-            options: "page_projectStoryEntry",
-        }
-    ],
-    
+        question_storyList
+    ],  
 };
-
-var page_projectStoryEntry = {
-		id: "page_projectStoryEntry",
-	    questions: [
-	        question_projectStoryScenarioType,
-	        question_projectStoryScenarioCustomQuestion
-	    ]
-	};
 
 var question_projectStoryScenarioType = {
     id: "question_projectStoryScenarioType",
     type: "select",
-    choices: ["askMeAnything", "magicEars", "flyOnTheWall", "projectAspects", "customQuestion"]
+    choices: ["askMeAnything", "magicEars", "flyOnTheWall", "projectAspects", "customQuestion"],
     help: "projectStoryScenarioType"
 }
 
@@ -51,8 +44,16 @@ var question_projectStoryScenarioCustomQuestion = {
     type: "textArea",
 }
 
+var page_projectStoryEntry = {
+    id: "page_projectStoryEntry",
+    questions: [
+        question_projectStoryScenarioType,
+        question_projectStoryScenarioCustomQuestion
+    ]
+};
+
 var pageList = [
-    page_Introduction,
-    page_ProjectStoriesIntro,
-    page_ProjectStoryEntry,
+    page_generalIntro,
+    page_projectStoriesIntro,
+    page_projectStoryEntry,
 ];
--- translations.js
+++ translations.js
@@ -3,7 +3,7 @@ var translations = {
 		"page_generalIntro_title": "Introduction",
 		"page_generalIntro_text": "<p>Hello and welcome to NarraCoach.</p>"
 			+ "<p>This application will guide you through carrying out a participatory narrative inquiry (PNI) project.</p>"
-			+ "<p>TYour project will go through these steps:</p>"
+			+ "<p>Your project will go through these steps:</p>"
 			+ "<ul><li>Planning: Tell stories and make sense of your goals and hopes for your project.</li>"
 			+ "<li>Collection: Set up your questions and collect stories in person or over the web.</li>"
 			+ "<li>Catalysis: Prepare some \"food for thought\" for people to use in working with the stories you have collected.</li>"
@@ -12,7 +12,7 @@ var translations = {
 			+ "<li>Return: Gather information about your project -- impact and reflect on what you have learned.",
 		// page: project stories
 		"page_projectStoriesIntro_title": "Project Stories Introduction",
-		"page_projectStoriesIntro_text": "The best way to start planning a PNI project is to tell stories about the project. Tell your stories as future histories, as if your project had already taken place.,
+		"page_projectStoriesIntro_text": "The best way to start planning a PNI project is to tell stories about the project. Tell your stories as future histories, as if your project had already taken place.",
 		// question: project story scenario
 		"question_projectStoryScenarioType": "Please choose one of these questions, which you will answer by describing a <b>scenario</b>.",
 		"question_projectStoryScenarioType_choice_askMeAnything": "Ask me anything",
@@ -23,6 +23,8 @@ var translations = {
 		// question: custom scenario question (only asked if they chose custom question option above)
 		"question_projectStoryScenarioTypeCustomQuestion": "Please enter the question to which this project story is an answer.",
 		// widget: story list (user can review stories entered, and (in this case) enter new stories)
-		"widget_storyList": ""
-		
+		"widget_storyList": "",
+		// ???
+		"page_projectStoryEntry_title": "TODO: page_projectStoryEntry",
+		"page_projectStoryEntry_text": "TODO: page_projectStoryEntry -- Enter a story about your project",
 }
