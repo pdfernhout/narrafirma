f16e74b52d1b79b348b1cd85399c4a5eb1f1f9ed
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-08-31 13:27:06 -0400

    Moving functionality to create table from page_general-information-about-project-participants.js into narracoach.js and removing file.

--- narracoach/narracoach.js
+++ narracoach/narracoach.js
@@ -13,7 +13,6 @@ require([
     "dojo/hash",
     "narracoach/page_design-questions",
     "narracoach/page_export-survey",
-    "narracoach/page_general-information-about-project-participants",
     "narracoach/page_graph-results",
     "narracoach/page_project-story-list",
     "narracoach/page_take-survey",
@@ -23,7 +22,9 @@ require([
     "dojo/string",
     "narracoach/widgets",
     "dijit/layout/ContentPane",
+    "dijit/form/Select",
     "dijit/layout/TabContainer",
+    "dojox/layout/TableContainer",
     "dojo/domReady!"
 ], function(
 	array,
@@ -33,7 +34,6 @@ require([
 	hash,
     page_designQuestions,
     page_exportSurvey,
-    page_generalInformationAboutProjectParticipants,
     page_graphResults,
     page_projectStoryList,
     page_takeSurvey,
@@ -43,7 +43,9 @@ require([
     string,
     widgets,
     ContentPane,
-    TabContainer
+    Select,
+    TabContainer,
+    TableContainer
 ){
 	// TODO: Add page validation
 	// TODO: Add translations for GUI strings used here
@@ -77,6 +79,95 @@ require([
     	alert("Unfinished");
     }
     
+    // page aspects table support -- start
+    
+	var groupHeader1 = new ContentPane({"content": "<i>N/A</i>", "colspan": 1});
+	var groupHeader2 = new ContentPane({"content": "<i>N/A</i>", "colspan": 1});
+	var groupHeader3 = new ContentPane({"content": "<i>N/A</i>", "colspan": 1});
+    
+	function updateRole1(newValue) {
+		if (newValue === "") newValue = "N/A";
+		groupHeader1.set("content", "<b><i>" + newValue + "</i></b>");
+	}
+
+	function updateRole2(newValue) {
+		if (newValue === "") newValue = "N/A";
+		groupHeader2.set("content", "<b><i>" + newValue + "</i></b>");
+	}
+	
+	function updateRole3(newValue) {
+		if (newValue === "") newValue = "N/A";
+		groupHeader3.set("content", "<b><i>" + newValue + "</i></b>");
+	}
+	
+	function newSpecialSelect(id, options) {
+		var theOptions = [];
+		array.forEach(options, function(option) {
+			theOptions.push({label: option, value: option});
+		});
+		var select = new Select({
+			id: id,
+	        options: theOptions,
+	        // TODO: Width should be determined from content using font metrics across all dropdowns
+	        width: "150px"
+	    });
+		return select;
+	}
+	
+    function insertPageAspectsTable(pseudoQuestion, pagePane) {
+    	console.log("insertPageAspectsTable", pseudoQuestion);
+    	
+    	var groupIDs = pseudoQuestion.options.split(",");
+    	
+    	registry.byId(groupIDs[0]).on("change", updateRole1);
+    	registry.byId(groupIDs[1]).on("change", updateRole2);
+    	registry.byId(groupIDs[2]).on("change", updateRole3);
+    	
+    	var questions = pageDefinitions["page_aspectsTable"].questions;
+    	
+		var table = new TableContainer({
+			cols: 4,
+			showLabels: false,
+		});
+		
+		var columnHeader1ContentPane = new ContentPane({"content": "<i>Question</i>", "colspan": 1, "align": "right"});
+		table.addChild(columnHeader1ContentPane);
+		
+		table.addChild(groupHeader1);
+		
+		table.addChild(groupHeader2);
+		
+		table.addChild(groupHeader3);
+		
+		var index = 0;
+		array.forEach(questions, function(question) {
+			console.log("question", question);
+			if (question.type === "header") {
+				var content = "<b>" + question.text + "</b>";
+				var headerContentPane = new ContentPane({"content": content, "colspan": 4, "align": "center"});
+				table.addChild(headerContentPane);
+			} else {
+				var questionContentPane = new ContentPane({"content": question.text, "colspan": 1, "align": "right"});
+				table.addChild(questionContentPane);
+				
+				// TODO: Translation
+				// var options = question.options.replace(/;/g, "\n");
+				var options = question.options.split(";");
+
+				// TODO: Maybe should do this to get styling and div id right? questionEditor.insertQuestionIntoDiv(question, page.containerNode);
+				table.addChild(newSpecialSelect(question.id + "_" + 1, options));
+				table.addChild(newSpecialSelect(question.id + "_" + 2, options));
+				table.addChild(newSpecialSelect(question.id + "_" + 3, options));
+			}
+		});
+		
+		pagePane.addChild(table);
+		
+		table.startup();
+    }
+    
+ // page aspects table support -- end
+    
     function createOrShowPage(id) {
     	if (currentPageID === id) return;
     	if (currentPageID) {
@@ -103,6 +194,8 @@ require([
 	    	   }
 	    	   if (question.type === "button") {
 	    		   widgets.newButton(question.id, question.text, pagePane.domNode, buttonUnfinishedClick);
+	    	   } else if (question.type === "page_aspectsTable") {
+	    		   insertPageAspectsTable(question, pagePane);
 	    	   } else if (questionEditor.supportedTypes.indexOf(question.type) === -1) {
 	    		   // Not supported yet
 	    		   question.text = "TODO: " + question.text;
@@ -172,6 +265,7 @@ require([
         array.forEach(pages, function(page) {
         	var title = page.name;
         	// TODO: Eventually remove legacy support for old way of defining pages
+        	// TODO: Eventually don't include popups or other special page types in list to display to user
         	var sections = title.split("-");
         	if (sections.length >= 2) {
         		title = sections[0];
@@ -194,9 +288,12 @@ require([
         	pageDefinitions[page.id] = page;
         	
         	// Make it easy to lookup previous and next pages from a page
-        	if (lastPageID) pageDefinitions[lastPageID].nextPageID = page.id;
-        	page.previousPageID = lastPageID;
-        	lastPageID = page.id;
+        	// Skip over special page types
+        	if (!page.type) {
+        		if (lastPageID) pageDefinitions[lastPageID].nextPageID = page.id;
+        		page.previousPageID = lastPageID;
+        		lastPageID = page.id;
+            }
         });
         
         /* TODO: Delete these pages after making sure any needed functionality is moved elsewhere (into widgets or more general code)
@@ -219,6 +316,7 @@ require([
     	domStyle.set(selectWidget.domNode, "width", "400px");
     	selectWidget.on("change", mainSelectChanged);
     	
+    	// TODO: Translation of buttons
     	widgets.newButton("previousPage", "Previous Page", "navigationDiv", previousPageClicked);
     	previousPageButton = registry.byId("previousPage");
     		
