ee46e2706567115d38998dd67987056f9c387be3
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-08-31 09:54:37 -0400

    Big layout change to use a select for navigation instead of tabs in narracoach.js

--- narracoach/narracoach.js
+++ narracoach/narracoach.js
@@ -7,6 +7,8 @@ var surveyResults = [];
 
 require([
     "dojo/_base/array",
+    "dojo/dom-construct",
+    "dojo/dom-style",
     "narracoach/page_design-questions",
     "narracoach/page_export-survey",
     "narracoach/page_general-information-about-project-participants",
@@ -17,12 +19,16 @@ require([
     "narracoach/page_take-survey",
     "narracoach/pages",
     "narracoach/question_editor",
+    "dijit/registry",
     "dojo/string",
+    "narracoach/widgets",
     "dijit/layout/ContentPane",
     "dijit/layout/TabContainer",
     "dojo/domReady!"
 ], function(
 	array,
+	domConstruct,
+	domStyle,
     page_designQuestions,
     page_exportSurvey,
     page_generalInformationAboutProjectParticipants,
@@ -33,25 +39,75 @@ require([
     page_takeSurvey,
     pages,
     questionEditor,
+    registry,
     string,
+    widgets,
     ContentPane,
     TabContainer
 ){
 	
+    var pageDefinitions = {};
+    var pageInstantiations = {};
+    var currentPageID = null;
+	
+    function mainSelectChanged(event) {
+    	var id = event;
+    	console.log("mainSelectChanged", id);
+    	createPage(id);
+    }
+    
+    function createPage(id) {
+    	if (currentPageID === id) return;
+    	if (currentPageID) {
+    		// var previousPage = pageDefinitions[currentPageID];
+    		domStyle.set(currentPageID, "display", "none");
+    	}
+    	
+    	var page = pageDefinitions[id];
+    	
+    	if (!pageInstantiations[id]) {
+
+	        var pagePane = new ContentPane({
+	        	"id": id,
+	            title: page.title,
+	            content: page.description.replace(/\n/g, "<br>\n"),
+	            style: "width: 100%",
+	       });
+	       
+	       array.forEach(page.questions, function(question) {
+	    	   if (question.type === "select" && question.options.indexOf(";") != -1) {
+	    		   // console.log("replacing select options", question.options);
+	    	       question.options = question.options.replace(/;/g, "\n");
+	    	       // console.log("result of replacement", question.options);
+	    	   }
+	    	   if (questionEditor.supportedTypes.indexOf(question.type) === -1) {
+	    		   // Not supported yet
+	    		   question.text = "TODO: " + question.text;
+	    		   question.text += " UNSUPPORTED TYPE OF: " + question.type + " with options: " + question.options + " on line: " + question.lineNumber;
+	    		   question.type = "header";
+	    	   }
+	    	   questionEditor.insertQuestionIntoDiv(question, pagePane.domNode);
+	       });
+	       
+	       pageInstantiations[id] = pagePane;
+	       pagePane.placeAt("pageDiv");
+	       pagePane.startup();    
+    	} else {
+    		// var previousPage = pageDefinitions[id];
+    		domStyle.set(id, "display", "block");
+    	}
+    	
+    	currentPageID = id;
+    }
+    
 	// Make all NarraCoach pages and put them in a TabContainer
     function createLayout() {
-        
-        var tabContainer = new TabContainer({
-            tabPosition: "left-h",
-            //tabPosition: "top",
-            style: "width: 100%",
-            // have the tab container height change to match internal panel
-            doLayout: false,
-        }, "tabContainerDiv");
-        
+    	console.log("createLayout start");
+    	var pageSelectOptions = [];
+    	
         var questionIndex = 0;
         
-        array.forEach(pages, function (page) {
+        array.forEach(pages, function(page) {
         	var title = page.name;
         	// TODO: Eventually remove legacy support for old way of defining pages
         	var sections = title.split("-");
@@ -67,32 +123,16 @@ require([
         	if (page.type) {
         		title += " SPECIAL: " + page.type;
         	}
-            var pagePane = new ContentPane({
-                title: title,
-                content: page.description.replace(/\n/g, "<br>\n"),
-                style: "width: 100%"
-           });
-           
-           array.forEach(page.questions, function(question) {
-        	   if (question.type === "select" && question.options.indexOf(";") != -1) {
-        		   // console.log("replacing select options", question.options);
-        	       question.options = question.options.replace(/;/g, "\n");
-        	       // console.log("result of replacement", question.options);
-        	   }
-        	   if (questionEditor.supportedTypes.indexOf(question.type) === -1) {
-        		   // Not supported yet
-        		   question.text = "TODO: " + question.text;
-        		   question.text += " UNSUPPORTED TYPE OF: " + question.type + " with options: " + question.options + " on line: " + question.lineNumber;
-        		   question.type = "header";
-        	   }
-        	   questionEditor.insertQuestionIntoDiv(question, pagePane.domNode);
-           });
-
-           tabContainer.addChild(pagePane); 
-           pagePane.startup();     	
+        	
+        	page.title = title;
+        	
+        	// Looks like Dojo select has a limitation where it can only take strings as values
+        	// so can't pass page in as value here and need indirect pageDefinitions lookup dictionary
+        	pageSelectOptions.push({label: title, value: page.id});
+        	pageDefinitions[page.id] = page;
         });
         
-        /*
+        /* TODO: Delete these pages after making sure any needed functionality is moved elsewhere (into widgets or more general code)
         page_introduction(tabContainer);
         page_projectStoriesIntro(tabContainer);
 
@@ -106,10 +146,19 @@ require([
 		page_graphResults(tabContainer);
 		*/
 
-        // Main startup
-        tabContainer.startup();
+    	widgets.newSelect("mainSelect", pageSelectOptions, null, "navigationDiv");
+       	//widgets.newSelect("mainSelect", null, "one\ntwo\nthree", "navigationDiv");
+    	
+    	var widget = registry.byId("mainSelect");
+    	console.log("widget", widget);
+    	widget.on("change", mainSelectChanged);
+    	
+    	// Setup the first page
+    	createPage(pages[0].id);
+    	
+    	console.log("createLayout end");
     }
-        
+    
     // TODO: Challenge of repeating sections....
 
     // Call the main function
