53d10ab2bcfdba1ab61d43d639cbd7a51d1e9e65
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-09-05 08:31:32 -0400

    Adding lipsum text to test stories in story browser.

--- narracoach/widget-story-browser.js
+++ narracoach/widget-story-browser.js
@@ -65,13 +65,22 @@ define([
     
     var testDogStories = [];
     
+    var lorumText = ": Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, " +
+    "totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. " +
+    "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores " +
+    "eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur," +
+    " adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem." +
+    " Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?" +
+    " Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum " +
+    "fugiat quo voluptas nulla pariatur?"
+    
     console.log("making test dog stories");
     for (var i = 0; i < 100; i++) {
         var newStory = {
             id: i,
             name: "name " + i,
             ownDog: true,
-            broughtHome: "Brought home " + i,
+            broughtHome: "Story " + i + lorumText,
             broughtHomeTitle: "Brought Home Title " + i,
             feeling1: i % 100,
             feeling2: i % 100,
