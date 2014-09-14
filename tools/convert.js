18dffb91d275d853d571f46d142dae82910e15cb
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-09-14 12:23:20 -0400

    Progress on reading in recommendations.csv into convert utility.

--- tools/convert.js
+++ tools/convert.js
@@ -7,14 +7,18 @@ require([
     "dojo/_base/array",
     "dojo/string",
     "dojo/text!../design/design_pages_notes.txt",
+    "dojo/text!../design/recommendations.csv",
     "dijit/form/Button",
+    "dojox/data/CsvStore",
     "dijit/form/SimpleTextarea",
     "dojo/domReady!"
 ], function(
     array,
     string,
     textOfDesign,
+    textOfRecommendations,
     Button,
+    CsvStore,
     SimpleTextarea
 ){
     
@@ -214,6 +218,69 @@ function addOutput(output) {
     outputTextArea.set("value", newValue);
 }
 
+function loadRecommendationsCSV() {
+    inputTextArea.set("value", textOfRecommendations);
+    outputTextArea.set("value", "");
+    
+    processRecommendationsCSV();
+}
+
+
+var matrix;
+var matrixColumnCount;
+var matrixRowCount;
+
+function getMatrixValue(row, column) {
+    if (row > matrixRowCount) return null;
+    if (column > matrixColumnCount) return null;
+    var line = matrix[row];
+    // console.log("test1", column, line.length, line);
+    if (column > line.length) return "";
+    return line[column];
+}
+
+function loadMatrix() {
+    // Load matrix
+    matrix = [];
+    matrixColumnCount = 0;
+    matrixRowCount = 0;
+    
+    var lines = textOfRecommendations.split("\r");
+    console.log(JSON.stringify(lines));
+    var rowCategoriesSplit = lines[0].split(",");
+    console.log(JSON.stringify(rowCategoriesSplit));
+    
+    for (var lineIndex in lines) {
+        var line = lines[lineIndex];
+        // TODO: Will not handle embedded commas or quotes
+        var splitLine = line.split(",");
+        matrix[lineIndex] = splitLine;
+        if (splitLine.length > matrixColumnCount) matrixColumnCount = splitLine.length;
+    }
+    matrixRowCount = lines.length;
+    console.log(matrixColumnCount, matrixRowCount, matrix);
+    console.log("test retrieval 5.5", getMatrixValue(5, 5));
+}
+
+function processRecommendationsCSV() {
+    // Process CSV file which has row categories line then row data line, then column category line, then column data line, then blank, then repeate columns
+    
+    // Store did not seem to load data carrectly based on line breaks not processed correctly despite changing them to \n; also could not find URL
+    //var fixedNewlinesText = textOfRecommendations.replace(/\r?\n/g, "\n");
+    // console.log("fixedNewlinesText", fixedNewlinesText);
+    // var store = new CsvStore({data: fixedNewlinesText});
+    // var store = new CsvStore({url: "file:../design/recommendations.csv"});
+    //console.log("store", store);
+    
+    loadMatrix();
+    
+    var rowCategories = [];
+    var columnCategories = [];
+    
+    
+
+}
+
 function createLayout() {
     var initialText = textOfDesign;
     
@@ -235,6 +302,8 @@ function createLayout() {
     
     newButton("Convert", "buttons", convert);
     newButton("Generate recommendations table", "buttons", generateRecommendationsTable);
+    
+    newButton("Load recommendations.csv", "buttons", loadRecommendationsCSV);
 }
 
 createLayout();
