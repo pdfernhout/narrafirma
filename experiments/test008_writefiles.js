1197903c3dd7285fba9392d253e63c91c834ec7f
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-10-25 11:18:53 -0400

    Added test of file writing with node.js and output file

---
+++ experiments/test008_writefiles.js
@@ -0,0 +1,10 @@
+// Test with node.js
+
+var fs = require('fs');
+fs.writeFile("WebContent/js/pages/test_nodejs_filewriting.txt", "Hello file!", function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("The file was saved!");
+    }
+}); 
