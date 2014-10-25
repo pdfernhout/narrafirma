bfff480247d32aa2a84025dbacf649e69320032e
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-10-25 11:20:48 -0400

    Added design_to_individual_pages.js for node.js as work in progress; added page_plannign_experimentjs as hand-coded output file example to work towards.

---
+++ WebContent/js/pages/page_planning_experiment.js
@@ -0,0 +1,48 @@
+/*
+    {
+        "id": "page_planning",
+        "name": "Planning",
+        "description": "",
+        "isHeader": true,
+        "questions": [
+            {
+                "id": "project_projectPlanningLabel",
+                "text": "In the planning phase of your PNI project, you will make decisions about how your project will proceed.\nYou will think about your goals, your topic, your participants, and any opportunities and dangers you might encounter during the project.",
+                "type": "label"
+            },
+            {
+                "id": "project_generalNotes_planning",
+                "text": "You can enter some general notes on planning in this project here.",
+                "shortText": "Project general notes",
+                "type": "textarea"
+            }
+        ]
+    },
+*/
+
+"use strict";
+
+define([
+        "js/widgets"
+    ], function(
+        widgets
+    ){      
+        function addWidgets(contentPane, model) {
+
+            // TODO: Text would go into translation table...
+            // "project_projectPlanningLabel": "In the planning phase of your PNI project, you will make decisions about how your project will proceed.\nYou will think about your goals, your topic, your participants, and any opportunities and dangers you might encounter during the project.",
+            widgets.addLabel(contentPane, "project_projectPlanningLabel", model);
+            
+            // "project_generalNotes_planning_prompt": "You can enter some general notes on planning in this project here.",
+            // "project_generalNotes_planning_header": "Project general notes",
+            widgets.addTextAreaQuestion(contentPane, "project_generalNotes_planning", model);
+        }
+
+        return {
+            "id": "page_planning",
+            "name": "Planning",
+            "isHeader": true,
+            "addWidgets": addWidgets
+        };
+        
+    });
