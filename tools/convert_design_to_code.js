7a47c8a307734ffde74218e5e2f66ff91ad5f67b
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-11-03 16:24:09 -0500

    Refactored code to parse design out of convert_design_to_code.js and into seperate parse-design.js module.

--- tools/convert_design_to_code.js
+++ tools/convert_design_to_code.js
@@ -1,3 +1,4 @@
+/*jslint node: true */
 "use strict";
 
 // Run with node.js
@@ -7,177 +8,10 @@
 // It generates and allPagesSummary.js which defines the domain data model and also has information about what pages go with what headers
 // It generates js/nls/pageMessages.js translation file
 
+var parseDesign = require("./parse-design.js");
+var pages = parseDesign.pages;
+
 var fs = require('fs');
-
-// Read design file
-var design = fs.readFileSync('design/design_pages_notes.txt', "utf8");
-
-function startsWith(str, prefix) {
-    // console.log("startsWith", prefix, str.lastIndexOf(prefix, 0) === 0, str);
-  return str.lastIndexOf(prefix, 0) === 0;
-}
-  
-function isString(something) {
-  return (typeof something == 'string' || something instanceof String);
-}
-
-var acceptableTypes = [
-  "text", "textarea", "label", "header", "select", "grid", "button",
-  "popup", "imageUploader", "recommendationTable", "questionsTable", "quizScoreResult",
-  "report", "checkboxes", "templateList", "checkboxesWithPull", "participantStoryForm", "storyBrowser",
-  "excerptsList", "storyThemer", "graphBrowser", "trendsReport", "clusterSpace", "listCount",
-  "questionAnswer", "questionAnswerCountOfTotalOnPage", "toggleButton", "boolean",
-  "observationsList", "image"
-];
-
-var usedIDs = {};
-var pages = [];
-
-function extract(line, rest, lineNumber) {
-  var lineSections = rest.split("[");
-  if (lineSections.length !== 2) {
-    console.log("FIX NEEDED: Expected two sections relating to opening bracket for line: " + lineNumber + " :: " + line);
-    lineSections = [rest, "]"];
-  }
-  
-  var lineContent = lineSections[0].trim();
-  var lineContentSections = lineContent.split('|');
-  var shortLineContent = null;
-  var longLineContent = lineContent;
-  if (lineContentSections.length == 2) {
-         shortLineContent = lineContentSections[0].trim();
-         longLineContent = lineContentSections[1].trim();
-     }
-  if (lineContentSections.length >= 3) console.log("FIX NEEDED: Too many segments in line content: " + lineNumber + " :: " + line);
-  
-  var infoSections = lineSections[1].split("]");
-  if (infoSections.length !== 2) {
-    console.log("FIX NEEDED: Expected two sections relating to closing bracket for line: " + lineNumber + " :: " + line);
-    infoSections = null;
-  }
-  
-  if (!infoSections) {
-      console.log("FIX NEEDED: Expected some content in brackets for line: " + lineNumber + " :: " + line);
-      infoSections = ["FIXME_" + lineNumber];
-  } else if (!infoSections[0]) {
-      console.log("FIX NEEDED: Expected a non-empty id for line: " + lineNumber + " :: " + line);
-      infoSections = ["FIXME_" + lineNumber];        
-  }
-  
-  var infoContentString = infoSections[0].trim();
-  var infoContentSplit = infoContentString.split("|");
-  var infoContent = {};
-  if (infoContentSplit.length >= 1) infoContent.id = infoContentSplit[0];
-  if (infoContentSplit.length >= 2) infoContent.type = infoContentSplit[1];
-  if (infoContentSplit.length >= 3) infoContent.options = infoContentSplit[2];
-  if (infoContentSplit.length >= 4) console.log("FIX NEEDED: Expected only up to three sections in brackets for line: " + lineNumber + " :: " + line);
-  
-  if (acceptableTypes.indexOf(infoContent.id) !== -1) {
-    console.log("WARNING: Using widget type as question ID for line: " + lineNumber + " :: " + line);
-    //if (!infoContent.type) infoContent.type = infoContent.id;
-    //infoContent.id = "FIXME_" + lineNumber;
-  }
-  
-  //if (infoContent.type && acceptableTypes.indexOf(infoContent.type) === -1) {
-  //    console.log("FIX NEEDED: Unknown type '" + infoContent.type + "' in brackets for line: " + lineNumber + " :: " + line);
-  //}
-  
-  if (!infoContent.id) {
-      console.log("WARNING: No id for line: " + lineNumber + " :: " + line);
-      infoContent.id = "FIXME_" + lineNumber;
-  }
-  
-  var previousLineNumber = usedIDs[infoContent.id];
-  if (previousLineNumber) {
-      console.log("FIX NEEDED: Widget ID '" + infoContent.id + "' was previously used on line " + previousLineNumber + " and is redeclared on line: " + lineNumber + " :: " + line);
-      infoContent.id = "FIXME_" + lineNumber;
-  } else {
-      usedIDs[infoContent.id] = lineNumber;
-  }
-  
-  return {"text": longLineContent, "shortText": shortLineContent, "info": infoContent};
-}
-
-function convert(input) {
-  console.log("convert");
-  // console.log("input", input);
-  
-  // Reset the list of IDs and output area in case the button is pressed twice without reloading
-  usedIDs = {};
-  allOutput = "";
-  
-  var lines = input.split("\n");
-  var lastPage = null;
-  var lastQuestion = null;
-  pages = [];
-  var lineNumber = 0;
-  var commentNumberInPage = 0;
-  var data;
-  
-  lines.forEach(function (line) {
-      lineNumber++;
-      // console.log("line", lineNumber, line);
-      line = line.trim();
-
-      var header = false;
-      var rest = line;
-      
-      if (startsWith(line, "#")) {
-          // A page or page section header
-          if (startsWith(line, "##")) {
-              // A page
-              rest = line.substring(2);
-          } else {
-              // A page as a section header
-              rest = line.substring(1);
-              header = true;
-          }
-          lastQuestion = null;
-          
-          data = extract(line, rest, lineNumber);
-          if (data) {
-              lastPage = {"id": data.info.id, "name": data.text, "description": "", "isHeader": header, "type": data.info.type, "options": data.info.options, "questions": []};
-              if (!lastPage.type) lastPage.type = "page";
-              pages.push(lastPage);
-              commentNumberInPage = 0;
-          }
-      } else if (lastPage && startsWith(line, "*")) {
-          rest = line.substring(1);
-          // A question
-          data = extract(line, rest, lineNumber);
-          if (data) {
-              if (!data.info.type) {
-                  console.log("WARNING: no type for line: " + lineNumber + " :: " + line);
-                  data.info.type = "label";
-              }
-              
-              if (acceptableTypes.indexOf(data.info.type) === -1) {
-                  console.log("WARNING: Unexpected type '" + data.info.type + "' for line: " + lineNumber + " :: " + line);
-              }
-              lastQuestion = {"id": data.info.id, "text": data.text, "shortText": data.shortText, "type": data.info.type, "options": data.info.options};
-              lastPage.questions.push(lastQuestion);
-          }
-      } else if (lastPage && startsWith(line, "//")) {
-          // console.log("comment line", line);
-          // if (lastPage.description) lastPage.description += "\n";
-          // lastPage.description += line;
-          // lastQuestion = {"id": "COMMENT_" + lastPage.id + "_" + (++commentNumberInPage), "text": line, "type": "label", "options": null};
-          // lastPage.questions.push(lastQuestion);
-      } else if (lastQuestion && line) {
-          if (lastQuestion.text) lastQuestion.text += "\n";
-          lastQuestion.text += line;
-      } else if (lastPage) {
-          if (lastPage.description) lastPage.description += "\n";
-          lastPage.description += line;
-      }
-      // console.log("line", header, stuff, name, "::", description);
-  });
-  
-  addOutput('"use strict";\n');
-  addOutput('define(\n');
-  addOutput(JSON.stringify(pages, null, "    "));
-  addOutput(');\n');
-}
 
 var allOutput = "";
 
@@ -186,9 +20,6 @@ function addOutput(output) {
 }
 
 // Main
-
-convert(design);
-// console.log("allOutput", allOutput);
 
 var fileTemplate = "// Generated from design\n" +
 "\"use strict\";\n" +
