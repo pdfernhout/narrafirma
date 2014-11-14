edb223491497f26a0920c722a8f2a8f1c9e783aa
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-11-14 13:33:48 -0500

    Improved calculation of survey to display in survey.js

--- WebContent/js/survey.js
+++ WebContent/js/survey.js
@@ -4,6 +4,7 @@ define([
     "js/domain",
     "dojo/dom-construct",
     "dojox/mvc/getPlainValue",
+    "dojo/_base/lang",
     "js/utility",
     "js/widgetBuilder",
     "dijit/layout/ContentPane",
@@ -14,6 +15,7 @@ define([
     domain,
     domConstruct,
     getPlainValue,
+    lang,
     utility,
     widgetBuilder,
     ContentPane,
@@ -35,6 +37,9 @@ define([
     }
     
     function takeSurvey() {
+        // TODO: Remove this -- ONLY FOR TESTING
+        domain.finalizeSurvey();
+        
         var surveyDialog = null;
         
         var form = new Form();
@@ -47,17 +52,33 @@ define([
         var contentPane = form.containerNode;
         
         console.log("domain.projectData", domain.projectData);
-        var questions = domain.projectData.exportedSurveyQuestions.project_storyQuestionsList;
+        var questions = [];
+
+        // TODO: Title, logo
+        // TODO: Improve
+        // TODO: Check how many in list...
+        var storySolicitationOuestion = domain.projectData.exportedSurveyQuestions.project_elicitingQuestionsList[0].elicitingQuestion_text;
+        var startText = domain.projectData.exportedSurveyQuestions.questionForm_startText;
+        var endText = domain.projectData.exportedSurveyQuestions.questionForm_endText;
+        if (startText) questions.push({storyQuestion_shortName: "startText", storyQuestion_text: startText, storyQuestion_type: "label"});
+        questions.push({storyQuestion_shortName: "story", storyQuestion_text: storySolicitationOuestion, storyQuestion_type: "textarea"});
+        questions.push({storyQuestion_shortName: "name", storyQuestion_text: "Please give your story a name", storyQuestion_type: "text"});
+        questions = questions.concat(domain.projectData.exportedSurveyQuestions.project_storyQuestionsList);
+        questions = questions.concat(domain.projectData.exportedSurveyQuestions.project_participantQuestionsList);
+        if (endText) questions.push({storyQuestion_shortName: "endText", storyQuestion_text: endText, storyQuestion_type: "label"});
+        console.log("questions", questions);
+        
         for (var questionIndex in questions) {
             var question = questions[questionIndex];
-            var id = question.storyQuestion_shortName;
-            var type = question.storyQuestion_type;
+            var id = question.storyQuestion_shortName || question.participantQuestion_shortName;
+            var type = question.storyQuestion_type || question.participantQuestion_type;
             // TODO: What to set for initial values?
-            model.set(id, null);
+            // TODO: Improve how making model...
+            // if (!(type in {label: 1, header: 1})) model.set(id, null);
             // TODO: Need better approach for translations; these could interfere with main application
-            domain.extraTranslations[id + "::prompt"] = question.storyQuestion_text;
+            domain.extraTranslations[id + "::prompt"] = question.storyQuestion_text || question.participantQuestion_text;
             var options = [];
-            var optionsString = question.storyQuestion_options;
+            var optionsString = question.storyQuestion_options || question.participantQuestion_options;
             if (optionsString) {
                 var splitOptions = optionsString.split("\n");
                 // Make sure options don't have leading or trailing space and are not otherwise blank
