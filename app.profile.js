6fe9b95ea596a38d9c6c0fece7b9dbc6447225db
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-12-04 15:30:43 -0500

    Adding app.profile.js and package.json as progress towards using Dojo build system updated TODO.txt

---
+++ app.profile.js
@@ -0,0 +1,10 @@
+"use strict";
+var profile = (function(){
+    return {
+        resourceTags: {
+            amd: function(filename, mid) {
+                return /\.js$/.test(filename);
+            }
+        }
+    };
+})();
