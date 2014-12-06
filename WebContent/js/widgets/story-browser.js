f9160cc4d9d4bcb3eba8f9e77ebe86bd0b3b60de
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-12-06 13:39:57 -0500

    Displaying participant info in story browser

--- WebContent/js/widgets/story-browser.js
+++ WebContent/js/widgets/story-browser.js
@@ -259,16 +259,43 @@ define([
         console.log("insertStoryBrowser start", id);
         
         // TODO: Fix this show also handles participant questions somehow
-        var questions = domain.getCurrentQuestionnaire().storyQuestions;
-        // console.log("DEBUG questions used by story browser", questions);
+        var questionnaire = domain.getCurrentQuestionnaire();
+        var storyQuestions = questionnaire.storyQuestions;
         
+        // TODO: What about idea of having IDs that go with eliciting questions so store reference to ID not text prompt?
+        var elicitingQuestionPrompts = [];
+        for (var elicitingQuestionIndex in questionnaire.elicitingQuestions) {
+            var elicitingQuestionSpecification = questionnaire.elicitingQuestions[elicitingQuestionIndex];
+            elicitingQuestionPrompts.push(elicitingQuestionSpecification.text);
+        }
+        
+        // TODO: Remove redundancy
+        var leadingStoryQuestions = [];
+        leadingStoryQuestions.unshift({id: "__survey_" + "storyName", shortName: "storyName", prompt: "Please give your story a name", type: "text", options:[]});
+        leadingStoryQuestions.unshift({id: "__survey_" + "storyText", shortName: "storyText", prompt: "Please enter your response to the question above in the space below", type: "textarea", options:[]});
+        leadingStoryQuestions.unshift({id: "__survey_" + "elicitingQuestion", shortName: "elicitingQuestion", prompt: "Please choose a question you would like to respond to", type: "select", options: elicitingQuestionPrompts});
+
+        // console.log("DEBUG questions used by story browser", questions);
+               
+        var questions = [].concat(leadingStoryQuestions, storyQuestions);
+        questions.push({id: "__survey_" + "participantData", shortName: "participantData", prompt: "---- participant data below ----", type: "header", options:[]});
         translate.addExtraTranslationsForQuestions(questions);
+        
+        translate.addExtraTranslationsForQuestions(questionnaire.participantQuestions);
+        
+        // TODO: add more participant and survey info, like timestamps and participant ID
         
         var popupPageDefinition = {
              "id": "storyBrowserQuestions",
              "questions": questions,
              buildPage: function (builder, contentPane, model) {
                  widgetBuilder.addQuestions(questions, contentPane, model);
+                 // TODO: Load correct participant data
+                 
+                 var participantID = model.get("_participantID");
+                 var participantData = domain.getParticipantDataForParticipantID(participantID);
+                 var participantDataModel = new Stateful(participantData);
+                 widgetBuilder.addQuestions(questionnaire.participantQuestions, contentPane, participantDataModel);
              }
         };
 
