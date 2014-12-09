12d028fd64821e68d9d2477c695fdc7837f2dc67
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2014-12-09 09:22:33 -0500

    Got recommendations file parsing to work; added recommendations-test.csv test file.

--- tools/parse-recommendations.js
+++ tools/parse-recommendations.js
@@ -1,3 +1,83 @@
+/*jslint node: true */
+"use strict";
+
+var recommendationsFileName = '../design/recommendations.csv';
+// var recommendationsFileName = './recommendations-test.csv';
+
+// Process Recommendations CSV file 
+// File should have category header line to define categories with "# SECTION" to define sections and blank items between sections
+// Each row should have a header (# SECTION) or item with entries for every regular column with a blank line between sections
+/* Example:
+"",    "#C1", "Ven1", "Ven2", "", "#C2", "Way1", "Way2"
+"#Q1",    "",     "",     "", "",    "",    "",      ""
+"op1",    "",    "1",    "2", "",    "",   "2",     "1"
+"op2",    "",    "3",    "4", "",    "",   "4",     "3"
+   "",    "",     "",     "", "",    "",    "",      ""
+"#Q2",    "",     "",     "", "",    "",    "",      ""
+"op1",    "",    "1",    "4", "",    "",   "3",     "1"
+"op2",    "",    "3",    "2", "",    "",   "4",     "1"
+*/
+
+/* The JSON output looks like:
+{
+  "Q1": {
+    "op1": {
+      "C1": {
+        "Ven1": "1",
+        "Ven2": "2"
+      },
+      "C2": {
+        "Way1": "2",
+        "Way2": "1"
+      }
+    },
+    "op2": {
+      "C1": {
+        "Ven1": "3",
+        "Ven2": "4"
+      },
+      "C2": {
+        "Way1": "4",
+        "Way2": "3"
+      }
+    }
+  },
+  "Q2": {
+    "op1": {
+      "C1": {
+        "Ven1": "1",
+        "Ven2": "4"
+      },
+      "C2": {
+        "Way1": "3",
+        "Way2": "1"
+      }
+    },
+    "op2": {
+      "C1": {
+        "Ven1": "3",
+        "Ven2": "2"
+      },
+      "C2": {
+        "Way1": "4",
+        "Way2": "1"
+      }
+    }
+  }
+}
+*/
+
+var fs = require('fs');
+
+function startsWith(str, prefix) {
+  return str.lastIndexOf(prefix, 0) === 0;
+}
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+/*
 function generateRecommendationsTable() {
     if (!pages) console.log('Run convert before generating recommendations table');
     outputTextArea.set("value", "");
@@ -15,14 +95,7 @@ function generateRecommendationsTable() {
     });
     
 }
-
-function loadRecommendationsCSV() {
-    inputTextArea.set("value", textOfRecommendations);
-    outputTextArea.set("value", "");
-    
-    processRecommendationsCSV();
-}
-
+*/
 
 var matrix = [];
 var matrixColumnCount = 0;
@@ -37,43 +110,101 @@ function getMatrixValue(row, column) {
     return line[column];
 }
 
-function loadMatrix() {
+function loadMatrixFromCSVText(text) {
     // Load matrix
     matrix = [];
     matrixColumnCount = 0;
     matrixRowCount = 0;
     
-    var lines = textOfRecommendations.split("\r");
-    console.log(JSON.stringify(lines));
-    var rowCategoriesSplit = lines[0].split(",");
-    console.log(JSON.stringify(rowCategoriesSplit));
+    var lines = text.split("\n");
+    // console.log(JSON.stringify(lines));
     
     for (var lineIndex in lines) {
         var line = lines[lineIndex];
         // TODO: Will not handle embedded commas or quotes
         var splitLine = line.split(",");
-        matrix[lineIndex] = splitLine;
-        if (splitLine.length > matrixColumnCount) matrixColumnCount = splitLine.length;
+        var lineItems = [];
+        for (var index in splitLine) {
+            var item = splitLine[index].trim();
+            if (startsWith(item, '"') && endsWith(item, '"')) {
+                // console.log("trimming item '%s'", item);
+                item = item.substring(1, item.length - 1);
+            }
+            lineItems.push(item);
+        }
+        
+        matrix.push(lineItems);
+        // if (lineItems.length > matrixColumnCount) matrixColumnCount = lineItems.length;
     }
-    matrixRowCount = lines.length;
-    console.log(matrixColumnCount, matrixRowCount, matrix);
-    console.log("test retrieval 5.5", getMatrixValue(5, 5));
+    matrixRowCount = matrix.length;
+    if (matrixRowCount > 0) matrixColumnCount = matrix[0].length;
+    
+    console.log("matrixColumnCount %s, matrixRowCount %s", matrixColumnCount, matrixRowCount);
+    // console.log("matrix", JSON.stringify(matrix, null, 2));
+    console.log("test retrieval 5,5", getMatrixValue(5, 5));
+    console.log("test retrieval 6,3", getMatrixValue(6, 6));
 }
 
-function processRecommendationsCSV() {
-    // Process CSV file which has row categories line then row data line, then column category line, then column data line, then blank, then repeate columns
-    
-    // Store did not seem to load data carrectly based on line breaks not processed correctly despite changing them to \n; also could not find URL
-    //var fixedNewlinesText = textOfRecommendations.replace(/\r?\n/g, "\n");
-    // console.log("fixedNewlinesText", fixedNewlinesText);
-    // var store = new CsvStore({data: fixedNewlinesText});
-    // var store = new CsvStore({url: "file:../design/recommendations.csv"});
-    //console.log("store", store);
-    
-    loadMatrix();
-    
-    var rowCategories = [];
-    var columnCategories = [];
-    
-    // TODO: finish
+function addRecommendation(recommendations, columnCategory, columnField, rowCategory, rowField, item) {
+    var question = recommendations[rowCategory];
+    if (!question) {
+        question = {};
+        recommendations[rowCategory] = question;
+    }
+    var option = question[rowField];
+    if (!option) {
+        option = {};
+        question[rowField] = option;
+    }
+    var category = option[columnCategory];
+    if (!category) {
+        category = {};
+        option[columnCategory] = category;
+    }
+    category[columnField] = item;
 }
+
+function processRecommendationsMatrix() {
+    var header = matrix[0];
+    var columnCategory = null;
+    var columnField = null;
+    var rowCategory = null;
+    var rowField = null;
+    
+    var recommendations = {};
+ 
+    for (var lineIndex = 1; lineIndex < matrixRowCount; lineIndex ++) {
+        var line = matrix[lineIndex];
+        if (lineIndex === 0) {
+            header = line;
+            continue;
+        }
+        rowField = getMatrixValue(lineIndex, 0).trim();
+        if (rowField === "") continue;
+        if (startsWith(rowField, "#")) {
+            rowCategory = rowField.substring(1).trim();
+            rowField = null;
+            continue;
+        }
+        
+        for (var columnIndex = 1; columnIndex <  matrixColumnCount; columnIndex++) {
+            columnField = header[columnIndex].trim();
+            if (columnField === "") continue;
+            if (startsWith(columnField, "#")) {
+                columnCategory = columnField.substring(1).trim();
+                columnField = null;
+                continue;
+            }
+            
+            var item = getMatrixValue(lineIndex, columnIndex).trim();
+            addRecommendation(recommendations, columnCategory, columnField, rowCategory, rowField, item);        
+        }
+    }
+    return recommendations;
+}
+
+console.log("Reading recommendations file:", recommendationsFileName);
+var textOfRecommendations = fs.readFileSync(recommendationsFileName, "utf8");
+loadMatrixFromCSVText(textOfRecommendations);
+var recommendations = processRecommendationsMatrix();
+console.log("recommendations", JSON.stringify(recommendations, null, 2));
