62d113d3c3ee22b69ca30fca0aff96885ba5468a
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-02-11 12:12:45 -0500

    Converting test data to list of stories with participant data merged in

--- WebContent/js/catalysis-main.js
+++ WebContent/js/catalysis-main.js
@@ -1,17 +1,19 @@
-"use strict";
-
 require([
     "dijit/layout/ContentPane",
     "dojo/dom",
     "lib/simple_statistics",
     "js/test-data",
+    "js/utility",
     "dojo/domReady!"
 ], function(
     ContentPane,
     dom,
     simpleStatistics,
-    testData
+    testData,
+    utility
 ){
+    "use strict";
+    
     console.log("simpleStatistics", simpleStatistics);
     
     var mainContentPane;
@@ -33,10 +35,44 @@ require([
        return childContentPane;
     }
     
+    function processResponses(responses) {
+        var stories = [];
+        for (var responseIndex in responses) {
+            var response = responses[responseIndex];
+            // console.log("response", response, response.content.stories);
+            for (var storyIndex in response.content.stories) {
+                var storyResponse = response.content.stories[storyIndex];
+                // console.log("story response", storyResponse);
+                var story = {};
+                for (var storyKey in storyResponse) {
+                    if (utility.startsWith(storyKey, "__survey_")) {
+                        var newStoryKey = storyKey.substr(9);
+                        if (!newStoryKey) newStoryKey = "MISSING";
+                        story["story_" + newStoryKey] = storyResponse[storyKey];
+                    }
+                }
+                story["storyID"] = storyResponse["_storyID"];
+                // Merge in participant data
+                for (var participantKey in response.content.participantData) {
+                    if (utility.startsWith(participantKey, "__survey_")) {
+                        var newParticipantKey = participantKey.substr(9);
+                        if (!newParticipantKey) newParticipantKey = "MISSING";
+                        story["participant_" + newParticipantKey] = response.content.participantData[participantKey];
+                    }
+                }
+                story["participantID"] = storyResponse["_participantID"];
+                stories.push(story);
+                console.log("Story with participant data", story);
+            }
+        }
+        return stories;
+    }
+    
     function createLayout() {
         console.log("createLayout");
         
-        console.log("test data", testData);
+        // console.log("test data", testData);
+        var stories = processResponses(testData.testSurveyResponses);
           
         var mainDiv = dom.byId("mainDiv");
         
