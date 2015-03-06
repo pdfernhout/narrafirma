e83486a7a3292fa48769bffe2030e01a58494086
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-03-06 15:59:44 -0500

    Removing unused (and duplicate) code from add_header.js

--- WebContent/js/panelBuilder/add_header.js
+++ WebContent/js/panelBuilder/add_header.js
@@ -11,13 +11,6 @@ define([
 ){
     "use strict";
     
- // TODO: Remove this -- just for testing/demo purposes
-    function randomHelpPageURL(id) {
-        var index = (Math.floor(Math.random() * 8) + 1);
-        var url = 'http://www.kurtz-fernhout.com/help100/0000000' + index + '.htm' + "#" + id;
-        return url;
-    }
-    
     function add_header(panelBuilder, contentPane, model, fieldSpecification) {
         var label = new ContentPane({
             content: panelBuilder.htmlForInformationIcon(panelBuilder.randomHelpPageURL(fieldSpecification.id)) + "&nbsp;&nbsp;" + "<b>" + translate("#" + fieldSpecification.id + "::prompt", fieldSpecification.displayPrompt) + "</b>"
