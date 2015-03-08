60d8c87b94c5a0a90a41c8288365da0a353394bd
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-03-08 09:37:59 -0400

    Turn FieldSpeficationCollection from a module into a class

--- WebContent/js/FieldSpecificationCollection.js
+++ WebContent/js/FieldSpecificationCollection.js
@@ -1,83 +1,87 @@
 define([
-    "js/applicationFieldSpecifications"
+   "dojo/_base/declare"
 ], function (
-    applicationFieldSpecifications
+   declare
 ) {
     "use strict";
     
-    // TODO: Optimize the lookup of questions for panels, and maybe others, by using a dictionary which caches a list
+    return declare([], {
+        
+        applicationFieldSpecifications: [],
     
-    function initialDataForField(fieldSpecification) {
-        var dataType = fieldSpecification.dataType;
-        if (dataType === "string") return "";
-        if (dataType === "array") return [];
-        if (dataType === "dictionary") return {};
-        if (dataType === "object") return {};
-        if (dataType === "boolean") return false;
-        throw new Error("Unsupported model field dataType: " + dataType);
-    }
-    
-    function buildModel(modelName) {
-        var model = {__type: modelName};
-        for (var i = 0; i < applicationFieldSpecifications.length; i++) {
-            var fieldSpecification = applicationFieldSpecifications[i];
-            if (fieldSpecification.model === modelName && fieldSpecification.dataType !== "none") {
-                model[fieldSpecification.id] = initialDataForField(fieldSpecification);
+        addFieldSpecifications: function(fieldSpecifications) {
+            for (var i = 0; i < fieldSpecifications.length; i++) {
+                var fieldSpecification = fieldSpecifications[i];
+                this.applicationFieldSpecifications.push(fieldSpecification);
             }
-        }
-        console.log("buildModel", modelName, model);
-        return model;
-    }
-    
-    function buildListOfPages() {
-        var pages = [];
-        for (var i = 0; i < applicationFieldSpecifications.length; i++) {
-            var fieldSpecification = applicationFieldSpecifications[i];
-            if (fieldSpecification.displayType === "page") {
-                pages.push(fieldSpecification);
+        },
+        
+        // TODO: Optimize the lookup of questions for panels, and maybe others, by using a dictionary which caches a list
+        
+        initialDataForField: function(fieldSpecification) {
+            var dataType = fieldSpecification.dataType;
+            if (dataType === "string") return "";
+            if (dataType === "array") return [];
+            if (dataType === "dictionary") return {};
+            if (dataType === "object") return {};
+            if (dataType === "boolean") return false;
+            throw new Error("Unsupported model field dataType: " + dataType);
+        },
+        
+        buildModel: function(modelName) {
+            var model = {__type: modelName};
+            for (var i = 0; i < this.applicationFieldSpecifications.length; i++) {
+                var fieldSpecification = this.applicationFieldSpecifications[i];
+                if (fieldSpecification.model === modelName && fieldSpecification.dataType !== "none") {
+                    model[fieldSpecification.id] = this.initialDataForField(fieldSpecification);
+                }
             }
-        }
-        return pages;
-    }
-    
-    function buildListOfPanels() {
-        var panels = [];
-        for (var i = 0; i < applicationFieldSpecifications.length; i++) {
-            var fieldSpecification = applicationFieldSpecifications[i];
-            if (fieldSpecification.displayType === "page" || fieldSpecification.displayType === "panel" ) {
-                panels.push(fieldSpecification);
+            console.log("buildModel", modelName, model);
+            return model;
+        },
+        
+        buildListOfPages: function() {
+            var pages = [];
+            for (var i = 0; i < this.applicationFieldSpecifications.length; i++) {
+                var fieldSpecification = this.applicationFieldSpecifications[i];
+                if (fieldSpecification.displayType === "page") {
+                    pages.push(fieldSpecification);
+                }
             }
-        }
-        return panels;
-    }
-    
-    function buildListOfQuestions() {
-        var questions = [];
-        for (var i = 0; i < applicationFieldSpecifications.length; i++) {
-            var fieldSpecification = applicationFieldSpecifications[i];
-            if (fieldSpecification.displayType !== "page" && fieldSpecification.displayType !== "panel" ) {
-                questions.push(fieldSpecification);
+            return pages;
+        },
+        
+        buildListOfPanels: function() {
+            var panels = [];
+            for (var i = 0; i < this.applicationFieldSpecifications.length; i++) {
+                var fieldSpecification = this.applicationFieldSpecifications[i];
+                if (fieldSpecification.displayType === "page" || fieldSpecification.displayType === "panel" ) {
+                    panels.push(fieldSpecification);
+                }
             }
-        }
-        return questions;
-    }
-    
-    function buildQuestionsForPanel(panelID) {
-        var questions = [];
-        for (var i = 0; i < applicationFieldSpecifications.length; i++) {
-            var fieldSpecification = applicationFieldSpecifications[i];
-            if (fieldSpecification.isHeader === undefined && fieldSpecification.displayPanel === panelID) {
-                questions.push(fieldSpecification);
+            return panels;
+        },
+        
+        buildListOfQuestions: function() {
+            var questions = [];
+            for (var i = 0; i < this.applicationFieldSpecifications.length; i++) {
+                var fieldSpecification = this.applicationFieldSpecifications[i];
+                if (fieldSpecification.displayType !== "page" && fieldSpecification.displayType !== "panel" ) {
+                    questions.push(fieldSpecification);
+                }
             }
+            return questions;
+        },
+        
+        buildQuestionsForPanel: function(panelID) {
+            var questions = [];
+            for (var i = 0; i < this.applicationFieldSpecifications.length; i++) {
+                var fieldSpecification = this.applicationFieldSpecifications[i];
+                if (fieldSpecification.isHeader === undefined && fieldSpecification.displayPanel === panelID) {
+                    questions.push(fieldSpecification);
+                }
+            }
+            return questions;
         }
-        return questions;
-    }
-    
-    return {
-        buildModel: buildModel,
-        buildListOfPages: buildListOfPages,
-        buildListOfPanels: buildListOfPanels,
-        buildQuestionsForPanel: buildQuestionsForPanel,
-        buildListOfQuestions: buildListOfQuestions,
-    };
+    });
 });
