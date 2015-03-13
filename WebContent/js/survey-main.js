7b935c2d097e73255840a7f64ade05b446c236a6
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-03-13 11:23:17 -0400

    Remove obsolete commented questionnaire cleanup code in survey-main.js

--- WebContent/js/survey-main.js
+++ WebContent/js/survey-main.js
@@ -26,36 +26,6 @@ require([
     // TODO: Fix hardcoded value
     var questionnaireID = 'questionnaire-test-003';
     
-    /*
-    function fixupQuestion(question) {
-        question.displayName = question.shortName;
-        question.displayType = question.type;
-        question.displayPrompt = question.prompt;
-        
-        var optionsAsArray;
-        var displayOptions;
-        if (question.options) {
-            optionsAsArray = question.options;
-            if (optionsAsArray.length === 1) {
-                displayOptions = optionsAsArray[0];
-                optionsAsArray = undefined;
-                if (displayOptions.indexOf("page_") === 0) displayOptions = displayOptions.replace("page_", "panel_");
-            }
-        }
-        if (optionsAsArray && (question.type !== "select" && question.type !== "checkboxes" && question.type !== "radiobuttons")) {
-            if (displayOptions) throw new Error("unexpected displayOptions and also dataOptions for: " + question.id);
-            displayOptions = optionsAsArray;
-            optionsAsArray = undefined;
-        }
-        
-        question.dataOptions = optionsAsArray;
-        question.displayOptions = displayOptions;
-
-        question.options = undefined;
-        question.type = undefined;
-    }
-    */
-    
     function createLayout() {
         console.log("createLayout");
            
@@ -68,21 +38,6 @@ require([
                 return;
             }
             console.log("got questionnaire from server", questionnaireID, questionnaire);
-            
-            /*
-            // Fixup old surveys (for testing)
-            console.log("!!! fixing up old survey for testing", questionnaire);
-            var i;
-            var question;
-            for (i = 0; i < questionnaire.participantQuestions.length; i++) {
-                question = questionnaire.participantQuestions[i];
-                fixupQuestion(question);
-            }
-            for (i = 0; i < questionnaire.storyQuestions.length; i++) {
-                question = questionnaire.storyQuestions[i];
-                fixupQuestion(question);
-            }
-            */
             
             var surveyDiv = dom.byId("surveyDiv");
             var form = survey.buildSurveyForm(questionnaire, finishedSurvey, false);
