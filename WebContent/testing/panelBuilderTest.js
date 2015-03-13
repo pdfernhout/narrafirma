28fff370747c87b6ec04644550b719d14a629aa1
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-03-13 15:54:52 -0400

    Improve panelBuilderTest to have a "panel" with questions

--- WebContent/testing/panelBuilderTest.js
+++ WebContent/testing/panelBuilderTest.js
@@ -8,6 +8,16 @@ require([
     
     console.log("panelBuilderTest.js");
     
+    var panelBuilder = new PanelBuilder();
+    
+    var contentPane = panelBuilder.newContentPane();
+    contentPane.placeAt("pageDiv").startup();
+    console.log("contentPane", contentPane);
+    
+    var model = {};
+    
+    console.log("model", model);
+
     var questions = [
         {
             id: "page_aboutYou",
@@ -32,16 +42,58 @@ require([
             displayPrompt: "How much experience do you have facilitating PNI projects?"
         },
     ];
-    
-    var panelBuilder = new PanelBuilder();
-    
-    var contentPane = panelBuilder.newContentPane();
-    contentPane.placeAt("pageDiv").startup();
-    console.log("contentPane", contentPane);
-
-    var model = {};
+   
     
     panelBuilder.addQuestions(questions, contentPane, model);
     
-    console.log("model", model);
+    var panel1 = {
+        id: "panel_addResonantStory",
+        displayName: "Add resonant story",
+        displayType: "panel",
+        section: "sensemaking",
+        modelClass: "ResonantStoryModel",
+        questions: [
+            {
+                id: "sensemakingSessionRecord_resonantStory_selection",
+                dataType: "none",
+                displayType: "storiesList",
+                displayName: "Resonant story",
+                displayPrompt: "Choose a story to mark as a resonant story for this sensemaking session."
+            },
+            {
+                id: "sensemakingSessionRecord_resonantStory_type",
+                dataType: "string",
+                dataOptions: ["pivot","voice","discovery","other"],
+                required: true,
+                displayType: "select",
+                displayName: "Type",
+                displayPrompt: "Which type of resonant story is this?"
+            },
+            {
+                id: "sensemakingSessionRecord_resonantStory_reason",
+                dataType: "string",
+                required: true,
+                displayType: "textarea",
+                displayName: "Why",
+                displayPrompt: "Why did this story stand out?"
+            },
+            {
+                id: "sensemakingSessionRecord_resonantStory_groups",
+                dataType: "string",
+                required: true,
+                displayType: "text",
+                displayName: "Groups",
+                displayPrompt: "For which participant groups was this story important?"
+            },
+            {
+                id: "sensemakingSessionRecord_resonantStory_notes",
+                dataType: "string",
+                displayType: "textarea",
+                displayName: "Notes",
+                displayPrompt: "Would you like to make any other notes about this story?"
+            }
+        ]
+    };
+    
+    panelBuilder.buildPanel(panel1, contentPane, model);
 });
