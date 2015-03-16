a096373c02d95849ae86a9cdea9f6546f49ecdd0
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-03-16 07:08:29 -0400

    Create modelSaveLoadTest based on modelCreationTest

---
+++ WebContent/testing/modelSaveLoadTest.js
@@ -0,0 +1,39 @@
+require([
+    "js/panelBuilder/PanelSpecificationCollection",
+    "dojo/text!js/applicationPanelSpecifications/planning/page_aboutYou.json",
+    "dojo/domReady!"
+], function(
+    PanelSpecificationCollection,
+    aboutYouPanelSpecificationText
+){
+    "use strict";
+    
+    console.log("modelSaveLoadTest.js");
+    
+    // console.log("aboutYouPanelSpecificationText", aboutYouPanelSpecificationText);
+    
+    var aboutYouPanelSpecification = JSON.parse(aboutYouPanelSpecificationText);
+    
+    console.log("aboutYouPanelSpecification", aboutYouPanelSpecification);
+    
+    console.log("aboutYouPanelSpecification.modelClass", aboutYouPanelSpecification.modelClass);
+    
+    aboutYouPanelSpecification.modelClass = "TestModel";
+    console.log("aboutYouPanelSpecification.modelClass", aboutYouPanelSpecification.modelClass);
+    
+    var panels = new PanelSpecificationCollection();
+    
+    panels.addPanelWithFields(aboutYouPanelSpecification);
+    
+    try {
+        var modelMissing = panels.buildModel("ProjectModel");
+        console.log("modelMissing (should not get here)", modelMissing);
+    } catch (e) {
+        console.log("missing model throws error OK", e);
+    }
+    
+    var testModel = panels.buildModel("TestModel");
+    
+    console.log("testModel", testModel);
+    
+});
