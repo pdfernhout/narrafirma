d4110c3e6ac862c077e5b76554a1f602ae45bcc3
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-03-16 15:43:46 -0400

    Add working store and load latest buttons to panelWithModelSaveLoadTest

--- WebContent/testing/panelWithModelSaveLoadTest.js
+++ WebContent/testing/panelWithModelSaveLoadTest.js
@@ -1,12 +1,16 @@
 require([
     "dojo/text!js/applicationPanelSpecifications/planning/page_aboutYou.json",
+    "js/panelBuilder/PanelBuilder",
     "js/panelBuilder/PanelSpecificationCollection",
     "js/pointrel20141201Client",
+    "dojo/Stateful",
     "dojo/domReady!"
 ], function(
     aboutYouPanelSpecificationText,
+    PanelBuilder,
     PanelSpecificationCollection,
-    pointrel20141201Client
+    pointrel20141201Client,
+    Stateful
 ){
     "use strict";
     
@@ -51,37 +55,84 @@ require([
         
         var aboutYouPanelSpecification = JSON.parse(aboutYouPanelSpecificationText);
         
+        console.log("aboutYouPanelSpecification", aboutYouPanelSpecification);
+        
         aboutYouPanelSpecification.modelClass = "TestModel";
         
         var panels = new PanelSpecificationCollection();
         
         panels.addPanelSpecification(aboutYouPanelSpecification);
         
-        var testModel = panels.buildModel("TestModel");
+        var testModelTemplate = panels.buildModel("TestModel");
+        
+        var testModel = new Stateful(testModelTemplate);
         
         console.log("testModel", testModel);
         
-        testModel.aboutYou_experience = "none";
-        testModel.aboutYou_help = "a lot";
-        testModel.aboutYou_tech = "some";
+        var panelBuilder = new PanelBuilder({panelSpecificationCollection: panels});
         
-        console.log("testModel updated", testModel);
+        var contentPane = panelBuilder.newContentPane();
+        contentPane.placeAt("pageDiv").startup();
+        
+        panelBuilder.buildPanel("page_aboutYou", contentPane, testModel);
+        
+        var loadLatestButtonSpecification = {
+            id: "loadButton",
+            displayType: "button",
+            displayName: "Load lastest",
+            displayPrompt: "Load latest",
+            displayConfiguration: loadLastestModelVersion,
+            displayPreventBreak: true
+        };
+        
+        var loadLatestButton = panelBuilder.buildField(contentPane, testModel, loadLatestButtonSpecification);
+        
+        var storeButtonSpecification = {
+            id: "storeButton",
+            displayType: "button",
+            displayName: "Store",
+            displayPrompt: "Store",
+            displayConfiguration: storeModelVersion
+        };
+        
+        var storeButton = panelBuilder.buildField(contentPane, testModel, storeButtonSpecification);
         
         // Thinking project ID should be a UUID?
         var projectID = "TestProject1234";
         var documentID = projectID + "-aboutYou";
         var committerID = "tester@example.com";
         
-        console.log("trying to store");
-        storeModel(testModel, documentID, null, committerID, function(error, sha256AndLength) {
-            console.log("store result", error, sha256AndLength);
-            if (!error) {
-                console.log("trying to load");
-                loadLatestModelVersion(documentID, function(error, content, envelope) {
-                    console.log("load result", error, content, envelope);
-                });
-            }
-        });
+        function loadLastestModelVersion() {
+            console.log("trying to load latest");
+            loadLatestModelVersion(documentID, function(error, latestModelVersion, envelope) {
+                console.log("load result", error, latestModelVersion, envelope);
+                if (error) {
+                    alert("An error happened when loading: " + error);
+                } else {
+                    console.log("Loaded OK");
+                    for (var key in testModelTemplate) {
+                        console.log("key", key);
+                        if (testModel.hasOwnProperty(key)) {
+                            console.log("Trying to copy", key);
+                            testModel.set(key, latestModelVersion[key]);
+                        }
+                    }
+                    alert("Loaded OK\n" + envelope.__sha256HashAndLength);
+                }
+            });
+        }
+        
+        function storeModelVersion() {
+            console.log("trying to store", testModel);
+            storeModel(testModel, documentID, null, committerID, function(error, sha256AndLength) {
+                console.log("store result", error, sha256AndLength);
+                if (error) {
+                    alert("An error happened when storing: " + error);
+                } else {
+                    alert("Stored OK\n" + sha256AndLength);
+                }
+            });
+        }
     }
 
     test();
