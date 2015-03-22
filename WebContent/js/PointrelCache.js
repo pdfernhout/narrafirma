1860b1dd2d6f41aee6ae89521e99dbbe23e7188e
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-03-22 10:05:11 -0400

    Renamed internal class in PointrelCache to match file name change.

--- WebContent/js/PointrelCache.js
+++ WebContent/js/PointrelCache.js
@@ -13,8 +13,8 @@ define([
     // TODO: change this default to 15 seconds
     var defaultCheckFrequency_milliseconds = 3000;
     
-    var DocumentCollection = function() {
-        console.log("constructing a DocumentCollection", this);
+    var PointrelCache = function() {
+        console.log("constructing a PointrelCache", this);
         this.lastCheckTime = null;
         this.frequencyOfChecks_milliseconds = defaultCheckFrequency_milliseconds;
         this.timer = null;
@@ -22,40 +22,40 @@ define([
         this.documents = {};
     };
     
-    DocumentCollection.prototype.startup = function() {
-        console.log(new Date().toISOString(), "starting up DocumentCollection", this);
+    PointrelCache.prototype.startup = function() {
+        console.log(new Date().toISOString(), "starting up PointrelCache", this);
         this.startTimer();
     };
     
-    DocumentCollection.prototype.shutdown = function() {
-        console.log(new Date().toISOString(), "shutting down DocumentCollection", this);
+    PointrelCache.prototype.shutdown = function() {
+        console.log(new Date().toISOString(), "shutting down PointrelCache", this);
         this.stopTimer();
     };
         
-    DocumentCollection.prototype.startTimer = function() {
+    PointrelCache.prototype.startTimer = function() {
         // Stop the timer in case it was running already
         // TODO: Is this really safe to do if the timer has already rang?
         this.stopTimer();
         this.timer = window.setTimeout(lang.hitch(this, this.timerRang), this.frequencyOfChecks_milliseconds);
     };
     
-    DocumentCollection.prototype.stopTimer = function() {
+    PointrelCache.prototype.stopTimer = function() {
         if (this.timer) {
             window.clearTimeout(this.timer);
             this.timer = null;
         }
     };
 
-    DocumentCollection.prototype.timerRang = function() {
+    PointrelCache.prototype.timerRang = function() {
         // TODO: Fill this in
         console.log(new Date().toISOString(), "should do check now", this);
         this.startTimer();
     };
   
-    DocumentCollection.prototype.track = function(documentID) {
+    PointrelCache.prototype.track = function(documentID) {
         // TODO: Fill this in
         
     };
   
-    return DocumentCollection;
+    return PointrelCache;
 });
