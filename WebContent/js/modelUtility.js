b0f83768e979dac4beed3105865564f44d86851c
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-03-24 09:11:24 -0400

    Add configuration options for copying new data to modelUtility.updateModelWithNewValues

--- WebContent/js/modelUtility.js
+++ WebContent/js/modelUtility.js
@@ -2,28 +2,30 @@ define([], function() {
     "use strict";
     
     return {
-        updateModelWithNewValues: function(model, newValues) {
+        updateModelWithNewValues: function(model, newValues, copyOnlyModelFieldsFlag, removeOtherFieldsFromModelFlag) {
             var key;
             
             // Copy new data into model
             for (key in newValues) {
-                if (newValues.hasOwnProperty(key)) {
+                if (newValues.hasOwnProperty(key) && (!copyOnlyModelFieldsFlag || model.get(key) !== undefined)) {
                     model.set(key, newValues[key]);
                 }
             }
             
-            // TODO: A little dangerous to remove stuff, should this extra data just be kept?
-            // Remove old data from model not in newValues
-            var fieldsToRemove = {};
-            for (key in model) {
-                if (model.hasOwnProperty(key) && !newValues.hasOwnProperty(key)) {
-                    // Stateful model adds "_watchCallbacks" so don't remove it
-                    if (!_.startsWith(key, "_")) fieldsToRemove[key] = true;
+            if (removeOtherFieldsFromModelFlag) {
+                // TODO: A little dangerous to remove stuff, should this extra data just be kept?
+                // Remove old data from model not in newValues
+                var fieldsToRemove = {};
+                for (key in model) {
+                    if (model.hasOwnProperty(key) && !newValues.hasOwnProperty(key)) {
+                        // Stateful model adds "_watchCallbacks" so don't remove it
+                        if (!_.startsWith(key, "_")) fieldsToRemove[key] = true;
+                    }
                 }
-            }
-            for (key in fieldsToRemove) {
-                console.log("removing old field/data", key, model.get(key));
-                model.set(key, undefined);
+                for (key in fieldsToRemove) {
+                    console.log("removing old field/data", key, model.get(key));
+                    model.set(key, undefined);
+                }
             }
         }
     };  
