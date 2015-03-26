f27c9890b7c834f7f530471d9a8b5157216fa26c
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-03-26 16:15:22 -0400

    Improve formatting of code in add_quizScoreResult and remove out-of-date comments

--- WebContent/js/applicationWidgets/add_quizScoreResult.js
+++ WebContent/js/applicationWidgets/add_quizScoreResult.js
@@ -34,17 +34,20 @@ define([
     
     function add_quizScoreResult(panelBuilder, contentPane, model, fieldSpecification) {
         var dependsOn = fieldSpecification.displayConfiguration;
+        
         var calculate = lang.partial(calculate_quizScoreResult, panelBuilder.panelSpecificationCollection, model, dependsOn);
-     // TODO: Fix when refactor
+        
         var label = panelBuilder._add_calculatedText(panelBuilder, contentPane, fieldSpecification, calculate);
+        
         // TODO: Recalculating next two variables wheres they are also calculated in _add_calculatedText
         var baseText = translate(fieldSpecification.id + "::prompt", fieldSpecification.displayPrompt);
+        
         var updateInfo = {"id": fieldSpecification.id, "label": label, "baseText": baseText, "calculate": calculate};
+        
         // Ensure this value is recalculated when dependent questions change by using watch
         for (var dependsOnIndex in dependsOn) {
             var questionID = dependsOn[dependsOnIndex];
             // console.log("setting up watch on", questionID, "for", id, model);
-         // TODO: Fix when refactor
             var watcher = model.watch(questionID, lang.hitch(panelBuilder, panelBuilder.updateLabelUsingCalculation, updateInfo));
             
             // Kludge to get the label to free the watcher by calling remove when it is destroyed
