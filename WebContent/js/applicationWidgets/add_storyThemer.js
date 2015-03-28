8de646fdebe14799efb03be43ffbb51777475a04
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-03-28 11:28:01 -0400

    Make story themer update when survey results change or when questionnaire changes
    Also refactored big method into smaller methods

--- WebContent/js/applicationWidgets/add_storyThemer.js
+++ WebContent/js/applicationWidgets/add_storyThemer.js
@@ -2,24 +2,26 @@ define([
     "js/domain",
     'dojox/html/entities',
     "dojox/uuid/generateRandomUuid",
+    "dojo/_base/lang",
     "js/surveyCollection",
+    "dojo/topic",
     "js/panelBuilder/standardWidgets/GridWithItemPanel",
     "dijit/form/ComboBox",
     "dijit/layout/ContentPane",
     "dojo/store/Memory",
-    "dstore/Memory",
     "dojo/Stateful",
     "js/panelBuilder/widgetSupport"
 ], function(
     domain,
     entities,
     generateRandomUuid,
+    lang,
     surveyCollection,
+    topic,
     GridWithItemPanel,
     ComboBox,
     ContentPane,
     Memory,
-    MemoryDstore,
     Stateful,
     widgetSupport
  ){
@@ -27,158 +29,173 @@ define([
     
     // story themer support
     
+    function addThemeButtonPressed(themeEntryComboBox, storyThemesStore, allThemes) {
+        console.log("Button pressed", storyThemesStore);
+        
+        var themeText = themeEntryComboBox.get("value");
+        console.log("themeText", themeText);
+        if (!themeText) return;
+        
+        themeEntryComboBox.set("value", "");
+        
+        //Check if the theme exists
+        var existingTheme = null;
+        for (var i = 0; i < allThemes.length; i++) {
+            if (allThemes[i].name === themeText) {
+                existingTheme = allThemes[i];
+                break;
+            }
+        }
+        
+        // If the theme does not exist, create one and put into all themes correctly sorted by name
+        if (!existingTheme) {
+            var uuid = generateRandomUuid();
+            var newTheme = {id: uuid, name: themeText};
+            allThemes.push(newTheme);
+            existingTheme = newTheme;
+            allThemes.sort(function (a, b) {
+                if (a.name < b.name) return -1;
+                if (a.name > b.name) return 1;
+                return 0;
+            });
+        }
+        
+        // Check if theme already in list of added themes, and if so, delete it as we will later add it again at the bottom
+        for (var j = 0; j < storyThemesStore.data.length; j++) {
+            var themeInStore = storyThemesStore.data[j];
+            if (themeInStore.name === existingTheme.name) {
+                storyThemesStore.remove(themeInStore.id);
+                break;
+            }
+        }
+        
+        storyThemesStore.add(existingTheme);
+    }
+    
+    function buildThemerPanel(panelBuilder, contentPane, model) {    
+        // Encode all user-supplied text to ensure it does not create HTML issues
+        var storyName = entities.encode(model.get("__survey_storyName"));
+        var storyText = entities.encode(model.get("__survey_storyText"));
+        var otherFields = "";
+        
+        var currentQuestionnaire = domain.currentQuestionnaire;
+        
+        var storyQuestions = [];
+        if (currentQuestionnaire) storyQuestions = currentQuestionnaire.storyQuestions;
+        
+        for (var i = 0; i < storyQuestions.length; i++) {
+            var storyQuestion = storyQuestions[i];
+            // otherFields[storyQuestion.shortName] = model.get(storyQuestion.id);
+            if (!model.get(storyQuestion.id)) continue;
+            otherFields += storyQuestion.shortName + ": " + JSON.stringify(model.get(storyQuestion.id)) + "<br>";
+        }
+        
+        var participantQuestions = [];
+        if (currentQuestionnaire) participantQuestions = currentQuestionnaire.participantQuestions;
+        
+        for (i = 0; i < participantQuestions.length; i++) {
+            var participantQuestion = participantQuestions[i];
+            otherFields += participantQuestion.shortName + ": " + JSON.stringify(model.get(participantQuestion.id)) + "<br>";
+        }
+        
+        var storyPane = new ContentPane({
+            content:
+                "<b><h2>" + storyName + "</h2></b>" +
+                 storyText + "<br><br>" +
+                 otherFields
+        });
+        storyPane.placeAt(contentPane);
+        
+        var themesPane = new ContentPane();
+        themesPane.placeAt(contentPane);
+        
+        // TODO: Need to hook up this with stories somehow, especially given they are loaded and saved outside the project
+        // var testItem = {id: "test", name: "test"};
+        var storyThemes = [];
+        var storyThemesStore = GridWithItemPanel.newMemoryTrackableStore(storyThemes, "id");
+        
+        var configuration2 = {removeButton: true, moveUpDownButtons: true, includeAllFields: true};
+        var itemPanelSpecification2 = {
+            "id": "storyThemeQuestions",
+            panelFields: [
+                {id: "name", displayName: "Theme", displayPrompt: "Theme", displayType: "text", dataOptions:[]}
+            ]
+        };
+        var themeList = new GridWithItemPanel(panelBuilder, themesPane, "storyThemeList", storyThemesStore, itemPanelSpecification2, configuration2);
+        console.log("themeList", themeList);
+        
+        var allThemes = [];
+        var allThemesStore = new Memory({
+            data: allThemes,
+            idProperty: "id"
+        });
+        
+        var themeEntryComboBox = new ComboBox({
+            store: allThemesStore,
+            style: {width: "50%"}
+        });
+        themeEntryComboBox.placeAt(themesPane);
+        
+        var addThemeButton = widgetSupport.newButton(themesPane, "#button_addTheme|Add theme", lang.partial(addThemeButtonPressed, themeEntryComboBox, storyThemesStore, allThemes));
+        addThemeButton.placeAt(themesPane);
+    }
+    
+    function currentQuestionnaireChanged(storyThemerInstance, currentQuestionnaire) {
+        // Update filters
+        var questions = surveyCollection.collectQuestionsForCurrentQuestionnaire();
+        storyThemerInstance.itemPanelSpecification.panelFields = questions;
+        
+        // Update item panel in grid
+        storyThemerInstance.storyList.changeItemPanelSpecification(storyThemerInstance.itemPanelSpecification);
+    }
+    
+    function loadLatestStoriesFromServerChanged(storyThemerInstance, newEnvelopeCount, allStories) {
+        console.log("loadLatestStoriesFromServerChanged", storyThemerInstance, newEnvelopeCount, allStories);
+        if (!newEnvelopeCount) return;
+        // console.log("loadLatestStoriesFromServerChanged", storyBrowserInstance, allStories);
+        storyThemerInstance.dataStore.setData(allStories);
+        // Apparently, trackable stored don't send a general update message when you change their data, so explicitely set grid store here to force update
+        storyThemerInstance.storyList.grid.set("collection", storyThemerInstance.dataStore);
+    }
+    
     // TODO: Fix so the filters get updated as the story questions get changed
     function insertStoryThemer(panelBuilder, pagePane, model, id) {
         console.log("insertStoryThemer start", id);
         
         // TODO: Handle the fact that currentQuestionnaire may be null if this is the first page loaded, and also may update as topic
-        // TODO: Fix this show also handles participant questions somehow
-        var questionnaire = domain.currentQuestionnaire;
-        var storyQuestions = questionnaire.storyQuestions;
-        var participantQuestions = questionnaire.participantQuestions;
-        
-        // TODO: What about idea of having IDs that go with eliciting questions so store reference to ID not text prompt?
-        var elicitingQuestionPrompts = [];
-        for (var elicitingQuestionIndex = 0; elicitingQuestionIndex < questionnaire.elicitingQuestions.length; elicitingQuestionIndex++) {
-            var elicitingQuestionSpecification = questionnaire.elicitingQuestions[elicitingQuestionIndex];
-            elicitingQuestionPrompts.push(elicitingQuestionSpecification.text);
-        }
-        
-        // TODO: Remove redundancy
-        var leadingStoryQuestions = [];
-        leadingStoryQuestions.unshift({id: "__survey_storyName", displayName: "storyName", displayPrompt: "Please give your story a name", displayType: "text", dataOptions:[]});
-        leadingStoryQuestions.unshift({id: "__survey_storyText", displayName: "storyText", displayPrompt: "Please enter your response to the question above in the space below", displayType: "textarea", dataOptions:[]});
-        leadingStoryQuestions.unshift({id: "__survey_elicitingQuestion", displayName: "elicitingQuestion", displayPrompt: "Please choose a question you would like to respond to", displayType: "select", dataOptions: elicitingQuestionPrompts});
-
-        // console.log("DEBUG questions used by story browser", questions);
-               
-        var questions = [].concat(leadingStoryQuestions, storyQuestions);
-        questions.push({id: "__survey_" + "participantData", displayName: "participantData", displayPrompt: "---- participant data below ----", displayType: "header", dataOptions:[]});
-        
-        // TODO: add more participant and survey info, like timestamps and participant ID
+        var questions = surveyCollection.collectQuestionsForCurrentQuestionnaire();
         
         var itemPanelSpecification = {
              id: "storyBrowserQuestions",
              panelFields: questions,
-             buildPanel: function (panelBuilder, contentPane, model) {
-                 var participantID = model.get("_participantID");
-                 var participantData = surveyCollection.getParticipantDataForParticipantID(participantID);
-                 var participantDataModel = new Stateful(participantData);
-                 
-                 /*
-                 panelBuilder.buildFields(questions, contentPane, model);
-                 // TODO: Load correct participant data
-                 
-                 panelBuilder.buildFields(questionnaire.participantQuestions, contentPane, participantDataModel);
-                 */
-                 
-                 // Encode all user-supplied text to ensure it does not create HTML issues
-                 var storyName = entities.encode(model.get("__survey_storyName"));
-                 var storyText = entities.encode(model.get("__survey_storyText"));
-                 var otherFields = "";
-                 
-                 for (var i = 0; i < storyQuestions.length; i++) {
-                     var storyQuestion = storyQuestions[i];
-                     // otherFields[storyQuestion.shortName] = model.get(storyQuestion.id);
-                     if (!model.get(storyQuestion.id)) continue;
-                     otherFields += storyQuestion.shortName + ": " + JSON.stringify(model.get(storyQuestion.id)) + "<br>";
-                 }
-                 for (i = 0; i < participantQuestions.length; i++) {
-                     var participantQuestion = participantQuestions[i];
-                     // otherFields[participantQuestion.shortName] = model.get(participantQuestion.id);
-                     if (!participantDataModel.get(participantQuestion.id)) continue;
-                     otherFields += participantQuestion.shortName + ": " + JSON.stringify(participantDataModel.get(participantQuestion.id)) + "<br>";
-                 }
-                 
-                 var storyPane = new ContentPane({
-                     content:
-                         "<b><h2>" + storyName + "</h2></b>" +
-                          storyText + "<br><br>" +
-                          otherFields
-                 });
-                 storyPane.placeAt(contentPane);
-                 
-                 var themesPane = new ContentPane();
-                 themesPane.placeAt(contentPane);
-                 
-                 // TODO: Need to hook up this with stories somehow, especially given they are loaded and saved outside the project
-                 // var testItem = {id: "test", name: "test"};
-                 var storyThemes = [];
-                 var storyThemesStore = new MemoryDstore({data: storyThemes, idProperty: "id"});
-                 
-                 var configuration2 = {removeButton: true, moveUpDownButtons: true, includeAllFields: true};
-                 var itemPanelSpecification2 = {
-                     "id": "storyThemeQuestions",
-                     panelFields: [
-                         {id: "name", displayName: "Theme", displayPrompt: "Theme", displayType: "text", dataOptions:[]}
-                     ]
-                 };
-                 var themeList = new GridWithItemPanel(panelBuilder, themesPane, "storyThemeList", storyThemesStore, itemPanelSpecification2, configuration2);
-                 console.log("themeList", themeList);
-                 
-                 var allThemes = [];
-                 var allThemesStore = new Memory({
-                     data: allThemes,
-                     idProperty: "id"
-                 });
-                 
-                 var themeEntryComboBox = new ComboBox({
-                     store: allThemesStore,
-                     style: {width: "50%"}
-                 });
-                 themeEntryComboBox.placeAt(themesPane);
-                 
-                 var addThemeButton = widgetSupport.newButton(pagePane, "#button_addTheme|Add theme", function () {
-                     console.log("Button pressed", themeList, storyThemes, storyThemesStore);
-                     var themeText = themeEntryComboBox.get("value");
-                     console.log("themeText", themeText);
-                     if (!themeText) return;
-                     themeEntryComboBox.set("value", "");
-                     var existingTheme = null;
-                     for (var i = 0; i < allThemes.length; i++) {
-                         if (allThemes[i].name === themeText) {
-                             existingTheme = allThemes[i];
-                             break;
-                         }
-                     }
-                     if (!existingTheme) {
-                         var uuid = generateRandomUuid();
-                         var newTheme = {id: uuid, name: themeText};
-                         allThemes.push(newTheme);
-                         existingTheme = newTheme;
-                         allThemes.sort(function (a, b) {
-                             if (a.name < b.name) return -1;
-                             if (a.name > b.name) return 1;
-                             return 0;
-                         });
-                     }
-                     // Check if theme already in list of added theme, and if so, delete it and add it again at the bottom
-                     for (var j = 0; j < storyThemes.length; j++) {
-                         if (storyThemes[j].name === existingTheme.name) {
-                             // storyThemes.splice(j, 1); 
-                             storyThemesStore.remove(storyThemes[j].id);
-                             break;
-                         }
-                     }
-                     storyThemesStore.add(existingTheme);
-                 });
-                 
-                 addThemeButton.placeAt(themesPane);
-             }
+             buildPanel: buildThemerPanel 
         };
 
         var stories = domain.allStories;
 
         // Store will modify underlying array
-        var dataStore = new MemoryDstore({
-            data: stories,
-            idProperty: "_storyID"
-        });
+        var dataStore = GridWithItemPanel.newMemoryTrackableStore(stories, "_storyID");
         
         // Only allow view button for stories
         var configuration = {viewButton: true, navigationButtons: true, includeAllFields: ["__survey_storyName", "__survey_storyText"]};
         var storyList = new GridWithItemPanel(panelBuilder, pagePane, id, dataStore, itemPanelSpecification, configuration);
         storyList.grid.set("selectionMode", "single");
+        
+        var storyThemerInstance = {
+            itemPanelSpecification: itemPanelSpecification,
+            dataStore: dataStore, 
+            storyList: storyList
+        };
+        
+        var loadLatestStoriesFromServerSubscription = topic.subscribe("loadLatestStoriesFromServer", lang.partial(loadLatestStoriesFromServerChanged, storyThemerInstance));
+        
+        // TODO: Kludge to get this other previous created widget to destroy a subscription when the page is destroyed...
+        pagePane.own(loadLatestStoriesFromServerSubscription);
+        
+        var currentQuestionnaireSubscription = topic.subscribe("currentQuestionnaire", lang.partial(currentQuestionnaireChanged, storyThemerInstance));
+        
+        // TODO: Kludge to get this other previous created widget to destroy a subscription when the page is destroyed...
+        pagePane.own(currentQuestionnaireSubscription);
         
         console.log("insertStoryThemer finished");
     }
