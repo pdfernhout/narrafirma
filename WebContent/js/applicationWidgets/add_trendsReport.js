b0924f93ca0de6a0c38a1f0ed1d916aa7e461cd9
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-04-03 16:15:52 -0400

    Make stub buildPatternList in add_trendsReport.js

--- WebContent/js/applicationWidgets/add_trendsReport.js
+++ WebContent/js/applicationWidgets/add_trendsReport.js
@@ -5,12 +5,22 @@ define([
 ){
     "use strict";
     
+    function buildPatternList() {
+        var result = [];
+        
+        // TODO: create all supported graphable permutations of questions
+
+        return result;
+    }
+    
     function add_trendsReport(panelBuilder, contentPane, model, fieldSpecification) {
         var questionContentPane = panelBuilder.createQuestionContentPaneWithPrompt(contentPane, fieldSpecification);
         
+        var patterns = buildPatternList();
+        
         var label = new ContentPane({
             // content: translate(id + "::prompt", fieldSpecification.displayPrompt)
-            content: "<b>UNFINISHED add_trendsReport: " + fieldSpecification.id + "</b>"             
+            content: "<b>UNFINISHED add_trendsReport: " + fieldSpecification.id + "</b>" + JSON.stringify(patterns)            
         });
         label.placeAt(questionContentPane);
         return label;
