a0f5199d9f495e0ad795cc46fd06cce1415d7193
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-05-06 09:30:52 -0400

    Updated add_questionAnswer.js to use project module and newer Pointrel approach
    TODO: Still have harcoded project ID.

--- WebContent/js/applicationWidgets/add_questionAnswer.js
+++ WebContent/js/applicationWidgets/add_questionAnswer.js
@@ -1,10 +1,10 @@
 define([
     "dojo/_base/lang",
-    "js/storage",
+    "js/project",
     "js/panelBuilder/translate"
 ], function(
     lang,
-    storage,
+    project,
     translate
 ){
     "use strict";
@@ -58,31 +58,30 @@ define([
         var referencedQuestionID = fieldSpecification.displayConfiguration;
         if (!referencedQuestionID) throw new Error("missing referencedQuestionID for field: " + fieldSpecification.id + " all: " + JSON.stringify(fieldSpecification));
 
-        // var calculate = lang.partial(calculate_questionAnswer, panelBuilder, model, referencedQuestionID);
+        var calculate = function () {
+            var triple = project.queryLatest("test-project", referencedQuestionID, undefined);
+            console.log("got triple for query", referencedQuestionID, triple, project);
+            if (triple) {
+                return triple.c;
+            }
+            return "";
+        };
         
-        var label = panelBuilder._add_calculatedText(panelBuilder, contentPane, fieldSpecification, function() {return div_for_value(null);});
+        var label = panelBuilder._add_calculatedText(panelBuilder, contentPane, fieldSpecification, function() {return div_for_value(calculate());});
         
         // TODO: Recalculating next two variables wheres they are also calculated in _add_calculatedText
         var baseText = translate(fieldSpecification.id + "::prompt", fieldSpecification.displayPrompt);
         
-        // var updateInfo = {"id": fieldSpecification.id, "label": label, "baseText": baseText, "calculate": calculate};
+        var updateInfo = {"id": fieldSpecification.id, "label": label, "baseText": baseText, "calculate": calculate};
         
-        /*
-        var watcher = model.watch(referencedQuestionID, lang.hitch(panelBuilder, panelBuilder.updateLabelUsingCalculation, updateInfo));
+        // TODO: Fix hardcoded project ID
+        var watcher = project.subscribe("test-project", referencedQuestionID, undefined, function(triple, message) {
+            panelBuilder.updateLabelUsingCalculation(updateInfo);
+        });
         
         // Klugde to get the contentPane to free the watcher by calling remove when it is destroyed
         // This would not work if the content pane continued to exist when replacing this component
         contentPane.own(watcher);
-        */
-        
-        // TODO: This should be updated periodically, like every 15 seconds via a heartbeat? Or should listen for server changes somehow?
-        
-        // Request the field for the project from the server, and update the status when ready
-        storage.loadLatestValueForProjectField(referencedQuestionID, function(value) {
-            console.log("add_questionAnswer server query result", value);
-            var updateInfo = {"id": fieldSpecification.id, "label": label, "baseText": baseText, "calculate": function() {return div_for_value(value);}};
-            panelBuilder.updateLabelUsingCalculation(updateInfo);
-        });
         
         return label;
     }
