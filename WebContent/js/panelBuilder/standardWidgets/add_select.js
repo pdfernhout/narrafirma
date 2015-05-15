988c7d37a7d0d449356bad327c05739d7909d342
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-05-15 14:05:31 -0400

    Add support in add_select.js for getting list of options from another field
    Also clears the value if the options no longer include the current value

--- WebContent/js/panelBuilder/standardWidgets/add_select.js
+++ WebContent/js/panelBuilder/standardWidgets/add_select.js
@@ -16,39 +16,87 @@ define([
     "use strict";
     
     function add_select(panelBuilder, contentPane, model, fieldSpecification, addNoSelectionOption) {
+        // console.log("add_select", fieldSpecification.id, model, fieldSpecification);
         var questionContentPane = panelBuilder.createQuestionContentPaneWithPrompt(contentPane, fieldSpecification);
         
-        var options = [];
-        if (addNoSelectionOption) options.push({name: translate("#selection_has_not_been_made|(no selection)"), id: "", selected: true});
-        var choices = fieldSpecification.dataOptions;
-        if (choices) {
-            array.forEach(choices, function(each) {
-                var label;
-                // console.log("choice", id, each);
-                if (lang.isString(each)) {
-                    label = translate(fieldSpecification.id + "::selection:" + each, each);
-                    options.push({name: label, id: each});
-                } else {
-                    // TODO: Maybe bug in dojo select that it does not handle values that are not strings
-                    // http://stackoverflow.com/questions/16205699/programatically-change-selected-option-of-a-dojo-form-select-that-is-populated-b
-                    label = translate(fieldSpecification.id + "::selection:" + each.label, each.label);
-                    options.push({name: label, id: each.value});
+        var dataStore = new Memory({"data": []});
+
+        var select = new FilteringSelect({
+            store: dataStore,
+            searchAttr: "name",
+            // TODO: Work on validation...
+            required: false,
+            // style: "width: 100%"
+            value: at(model, fieldSpecification.id)
+        });
+        
+        var specifiedChoices = fieldSpecification.dataOptions;
+        var choices = specifiedChoices;
+        
+        if (_.isString(specifiedChoices)) {
+            // console.log("Indirect choices");
+            choices = model.get(specifiedChoices);
+            // Need to track when choices change; problematical if array?
+            var subscription = model.watch(specifiedChoices, function(name, oldChoices, newChoices) {
+                // console.log("name, oldChoices, newChoices", name, oldChoices, JSON.stringify(newChoices, null, 4));
+                // Should validate that the current choice is still acceptable -- although that would entail then changing the data
+                var currentValue = model.get(fieldSpecification.id);
+                // console.log("currentValue", currentValue, fieldSpecification, model);
+                var isValueInChoices = updateChoices(newChoices, model.get(fieldSpecification.id));
+                // console.log("isValueInChoices", isValueInChoices);
+                if (!isValueInChoices) {
+                    // TODO: Should this be null or an empty string?
+                    // TODO: Should the GUI really be enforcing this rule?
+                    model.set(fieldSpecification.id, "");
                 }
-            });           
+            });
+            select.own(subscription);
+        }
+        
+        if (choices) {
+            updateChoices(choices, model.get(fieldSpecification.id));       
         } else {
             console.log("No choices or options defined for select", fieldSpecification.id);
         }
         
-        var dataStore = new Memory({"data": options});
-        
-        var select = new FilteringSelect({
-                store: dataStore,
-                searchAttr: "name",
-                // TODO: Work on validation...
-                required: false,
-                // style: "width: 100%"
-                value: at(model, fieldSpecification.id)
-        });
+        function updateChoices(choices, currentValue) {
+            var options = [];
+            if (addNoSelectionOption) options.push({name: translate("#selection_has_not_been_made|(no selection)"), id: "", selected: true});
+
+            var isValueInChoices = false;
+            
+            array.forEach(choices, function(each) {
+                var label;
+                var value;
+                // console.log("choice", id, each);
+                if (lang.isString(each)) {
+                    label = translate(fieldSpecification.id + "::selection:" + each, each);
+                    options.push({name: label, id: each});
+                    if (currentValue === each) isValueInChoices = true;
+                } else {
+                    // TODO: Maybe bug in dojo select that it does not handle values that are not strings
+                    // http://stackoverflow.com/questions/16205699/programatically-change-selected-option-of-a-dojo-form-select-that-is-populated-b
+                    if (fieldSpecification.dataOptionValueKey) {
+                        value = each[fieldSpecification.dataOptionValueKey];
+                    } else {
+                        value = each.value;
+                    }
+                    if (fieldSpecification.displayDataOptionField) {
+                        label = each[fieldSpecification.displayDataOptionField];
+                    } else {
+                        label = value;
+                    }
+                    label = translate(fieldSpecification.id + "::selection:" + label, label);
+                    options.push({name: label, id: value});
+                    if (currentValue === value) isValueInChoices = true;
+                }
+            });
+            
+            dataStore.setData(options);
+            
+            // console.log("updateChoices", currentValue, isValueInChoices, (currentValue === null || currentValue === undefined || currentValue === ""));
+            return isValueInChoices || (currentValue === null || currentValue === undefined || currentValue === "");
+        }
         
         select.placeAt(questionContentPane);
         return select;
