499a68e896466a7ae16bffa93273699b12660825
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-05-26 13:25:30 -0400

    Remove out-of-date code from surveyCollection and add some TODO comments

--- WebContent/js/surveyCollection.js
+++ WebContent/js/surveyCollection.js
@@ -69,109 +69,9 @@ define([
        return questionnaire;
    }
    
-   // Can also just pass in callback as first arg, rest are unused and are for compatibility with GUI calling system
-   function loadLatestStoriesFromServer(contentPane, model, fieldSpecification, value, callback) {
-       console.log("loadLatestStoriesFromServer called");
-       if (lang.isFunction(contentPane)) callback = contentPane;
-       
-       storage.loadLatestSurveyResults(function (allEnvelopes, newEnvelopes) {
-           // console.log("load survey result", "all", allEntries, "new", newEntries);
-           var newEnvelopeCount = 0;
-           for (var index in newEnvelopes) {
-               var newEnvelopeReference = newEnvelopes[index];
-               var newEnvelope = allEnvelopes[newEnvelopeReference];
-               if (newEnvelope) {
-                   newEnvelopeCount++;
-                   var surveyResult = newEnvelope.content;
-                   if (surveyResult) {
-                       // TODO: Kludge to remove for working with test data
-                       if (!surveyResult.id) {
-                           // console.log("UUID issue", newEntries[index], surveyResult);
-                           // surveyResult.id = uuid();
-                           // Use the identifier for the resource the survey is in
-                           surveyResult.id = newEnvelopeReference;
-                       }
-                       domain.allCompletedSurveys.push(surveyResult);
-                       
-                       // Now add stories in survey to allStories, with extra participant information
-                       var stories = surveyResult.stories;
-                       for (var storyIndex in stories) {
-                           var story = stories[storyIndex];
-                           // console.log("=== story", story);
-                           
-                           // Add participant info for story
-                           var participantData = surveyResult.participantData;
-                           for (var key in participantData) {
-                               if (key !== "__type") {
-                                   story[key] = participantData[key];
-                               }
-                           }
-                           domain.allStories.push(story);
-                       }
-                   } else {
-                       console.log("ERROR: Missing surveyResult in newEnvelope", newEnvelope);
-                   }
-               } else {
-                   console.log("ERROR: Problem reading resourceContent from newEnvelope", newEnvelopeReference);
-               }
-           }
-           
-           if (newEnvelopeCount === 0) {
-               console.log("loadLatestStoriesFromServer: No new survey results were found.");
-               topic.publish("loadLatestStoriesFromServer", newEnvelopeCount, null);
-               return;
-           } else {
-               console.log("loadLatestStoriesFromServer: There were " + newEnvelopeCount + " new survey result(s) found.");
-           }
-           
-           console.log("===== All stories", domain.allStories);
-           
-           if (callback) callback(newEnvelopeCount);
-           
-           // Tell any listeners like story browsers that there are more stories
-           topic.publish("loadLatestStoriesFromServer", newEnvelopeCount, domain.allStories);
-           topic.publish("totalNumberOfSurveyResults", allEnvelopes.length);
-       });
-   }
-   
-   // TODO: Update references to these:
-   // topic.publish("loadLatestStoriesFromServer", newEnvelopeCount, domain.allStories);
-   // topic.publish("totalNumberOfSurveyResults", allEnvelopes.length);
-
-   function finalizeSurvey() {
-       questionnaireGeneration.generateQuestionnaire(function (questionnaire) {
-           var questionnaireID = domain.currentQuestionnaireID;
-           questionnaire.questionnaireID = questionnaireID;
-           
-           questionnaireGeneration.ensureAtLeastOneElicitingQuestion(questionnaire);
-           
-           domain.currentQuestionnaire = questionnaire;
-           
-           console.log("storyCollectionStart finalized questionnaire", questionnaire);
-           
-           storage.storeQuestionnaireVersion(questionnaireID, questionnaire, function(error) {
-               if (error) { return alert("Could not store questionnaire"); }
-               console.log("stored questionnaire", questionnaire);
-               alert("Stored questionnaire as:" + questionnaireID);
-           });
-       });
-   }
-       
-   function storyCollectionStart() {
-       console.log("storyCollectionStart");
-       
-       var questionnaireID = domain.currentQuestionnaireID;
-       var status = {questionnaireID: questionnaireID, active: true};
-       storage.storeQuestionnaireStatus(questionnaireID, status, function(error) {
-           console.log("Activated questionnaire", questionnaireID, status);
-           domain.questionnaireStatus = status;
-           topic.publish("isStoryCollectingEnabled", domain.questionnaireStatus);
-       });
-       
-       // TODO: Probably should have separate finalizing step
-       alert("also finalizing survey for testing...");
-       finalizeSurvey();
-   }
+   // TODO: Find all users of loadLatestStoriesFromServer and fix them
+   // TODO: Fix users of: topic.publish("loadLatestStoriesFromServer", newEnvelopeCount, domain.allStories);
+   // TODO: Fix users of: topic.publish("totalNumberOfSurveyResults", allEnvelopes.length);
    
    function storyCollectionStop() {
        var questionnaireID = domain.currentQuestionnaireID;
@@ -184,21 +84,9 @@ define([
    }
    
    function isStoryCollectingEnabled(question) {
-       return domain.questionnaireStatus.active;
-   }
-   
-   function loadCurrentQuestionnaire() {
-       // TODO: Fix hardcoded questionnaire ID
-       storage.loadLatestQuestionnaireVersion(domain.currentQuestionnaireID, function(error, questionnaire) {
-           if (error) {
-               // Don't alert, because it is possible nothing has been saved
-               console.log("Problem loading latest questionnaire version", error);
-           } else {
-               console.log("loadCurrentQuestionnaire", questionnaire);
-               domain.currentQuestionnaire = questionnaire;
-               topic.publish("currentQuestionnaire", domain.currentQuestionnaire);
-           }
-       });
+       // return domain.questionnaireStatus.active;
+       // TODO: Fix!
+       return true;
    }
    
    function collectQuestionsForQuestionnaire(questionnaire) {
@@ -258,14 +146,9 @@ define([
        getStoriesForStoryCollection: getStoriesForStoryCollection,
        getQuestionnaireForStoryCollection: getQuestionnaireForStoryCollection,
        
-       storyCollectionStart: storyCollectionStart,
        storyCollectionStop: storyCollectionStop,
-       
-       loadLatestStoriesFromServer: loadLatestStoriesFromServer,
-       
+
        isStoryCollectingEnabled: isStoryCollectingEnabled,
-       
-       loadCurrentQuestionnaire: loadCurrentQuestionnaire,
        
        collectQuestionsForQuestionnaire: collectQuestionsForQuestionnaire,
        
