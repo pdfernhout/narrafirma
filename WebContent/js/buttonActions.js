6c4404a9af03829f57e7657e62a3402126b5b81b
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-05-31 10:02:55 -0400

    Add stub support for importing questionnaire
    Refactored CSV import code to reduce redundancy for importing stories as
    well.

--- WebContent/js/buttonActions.js
+++ WebContent/js/buttonActions.js
@@ -1,5 +1,6 @@
 define([
     "js/panelBuilder/browser",
+    "lib/d3/d3",
     "js/panelBuilder/dialogSupport",
     "js/modelUtility",
     "js/navigationPane",
@@ -12,6 +13,7 @@ define([
     "dojo/domReady!"
 ], function(
     browser,
+    d3,
     dialogSupport,
     modelUtility,
     navigationPane,
@@ -298,14 +300,111 @@ define([
         alert(message);
     }
     
-    function processCSVFileContents(contents) {
-        console.log("processCSVFileContents contents", contents);
+    function processCSVContents(contents, callbackForItem) {
+        console.log("processCSVContents contents", contents);
         
-        throw new Error("Unfinished");
+        var rows = d3.csv.parseRows(contents);
+        console.log("rows", rows);
+        
+        var items = [];
+        var header;
+        
+        for (var rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+            var row = rows[rowIndex];
+            // Throw away initial blank lines and comment lines
+            if (!row.length || !row[0] || row[0].trim().charAt(0) === ";") {
+                console.log("comment", row[0]);
+            } else {
+                if (!header) {
+                    header = [];
+                    var headerEnded = false;
+                    for (var headerIndex = 0; headerIndex < row.length; headerIndex++) {
+                        var label = row[headerIndex];
+                        if (label) {
+                            // Should be an error if header fields are missing but more show up later
+                            if (headerEnded) {
+                                console.log("ERROR: header has empty field before end");
+                                alert("ERROR: header has empty field before end");
+                            }
+                            header.push(label);
+                        } else {
+                            headerEnded = true;
+                        }
+                    }
+                } else {
+                    var newItem = callbackForItem(header, row);
+                    if (newItem) items.push(newItem);
+                }
+            }
+        }
+        // console.log("header", header);
+        // console.log("items", items);
+        return {header: header, items: items};
     }
     
-    function importCSVFile() {
-        // console.log("importCSVFile");
+    function processCSVContentsForStories(contents) {
+        // Throws away line after header which starts with a blank
+        var headerAndItems = processCSVContents(contents, function (header, row) {
+            var newItem = {};
+            for (var fieldIndex = 0; fieldIndex < header.length; fieldIndex++) {
+                var fieldName = header[fieldIndex];
+                // TODO: Should the value really be trimmed?
+                var value = row[fieldIndex].trim();
+                if (newItem[fieldName] === undefined) {
+                    newItem[fieldName] = value;
+                } else {
+                    // Handle case where this is a multiple choice question, which is indicated by multiple columns
+                    var data = newItem[fieldName];
+                    if (data && typeof data === 'object') {
+                        if (value !== "") data[value] = true;
+                    } else {
+                       var newData = {};
+                       if (data !== "") newData[data] = true;
+                       if (value !== "") newData[value] = true;
+                       newItem[fieldName] = newData;
+                    }
+                }
+            }
+            return newItem;
+        });
+        console.log("processCSVContentsForStories headerAndItems", headerAndItems);
+    }
+    
+    function processCSVContentsForQuestionnaire(contents) {
+        var headerAndItems = processCSVContents(contents, function (header, row) {
+            console.log("callback", header, row);
+            var newItem = {};
+            var lastFieldIndex;
+            for (var fieldIndex = 0; fieldIndex < row.length; fieldIndex++) {
+                var fieldName = header[fieldIndex];
+                if (fieldName) {
+                    lastFieldIndex = fieldIndex;
+                } else {
+                    fieldName = header[lastFieldIndex];
+                }
+                // TODO: Should the value really be trimmed?
+                var value = row[fieldIndex].trim();
+                console.log("fieldName, value", fieldName, value);
+                if (fieldIndex < header.length - 1) {
+                    newItem[fieldName] = value;
+                } else {
+                    // Handle multiple values for last header items
+                    var list = newItem[fieldName];
+                    console.log("list", list, fieldIndex, fieldName);
+                    if (!list) {
+                        list = [];
+                        newItem[fieldName] = list;
+                    }
+                    if (value) list.push(value);
+                }
+            }
+            return newItem;
+        });
+        console.log("processCSVContentsForQuestionnaire headerAndItems", headerAndItems); 
+    }
+    
+    function chooseCSVFileToImport(callback) {
+        // console.log("chooseFileToImport");
         var cvsFileUploader = document.getElementById("csvFileLoader");
         // console.log("cvsFileUploader", cvsFileUploader);
         cvsFileUploader.onchange = function() {
@@ -316,11 +415,19 @@ define([
             var reader = new FileReader();
             reader.onload = function(e) {
                 var contents = e.target.result;
-                processCSVFileContents(contents);
+                callback(contents);
             };
             reader.readAsText(file);
         };
         cvsFileUploader.click();
+    }
+    
+    function importCSVStories() {
+        chooseCSVFileToImport(processCSVContentsForStories);
+    }
+    
+    function importCSVQuestionnaire() {
+        chooseCSVFileToImport(processCSVContentsForQuestionnaire);
     }
     
     function logoutButtonClicked() {
@@ -339,7 +446,8 @@ define([
         storyCollectionStop: surveyCollection.storyCollectionStop,
         copyStoryFormURL: copyStoryFormURL,
         guiOpenSection: guiOpenSection,
-        importCSVFile: importCSVFile,
+        importCSVQuestionnaire: importCSVQuestionnaire,
+        importCSVStories: importCSVStories,
 
         // Called directly from application
         importExportOld: importExportClicked,
