5e728743acd8bf3097f51bd7caa31d94037b9766
Cynthia Kurtz <cfkurtz@kurtz-fernhout.com>
2015-06-10 20:15:21 -0400

    Changed creation of "header" question element so that instead of making it bold it assigns a style. Set the style in the CSS file.

--- WebContent/js/panelBuilder/standardWidgets/add_header.js
+++ WebContent/js/panelBuilder/standardWidgets/add_header.js
@@ -6,12 +6,13 @@ define([
     "use strict";
     
     function add_header(panelBuilder, contentPane, model, fieldSpecification) {
-        var content = "<b>" + translate(fieldSpecification.id + "::prompt", fieldSpecification.displayPrompt) + "</b>";
+        var content = translate(fieldSpecification.id + "::prompt", fieldSpecification.displayPrompt);
         if (panelBuilder.addHelpIcons) {
             content = panelBuilder.htmlForInformationIcon(panelBuilder.helpPageURLForField(fieldSpecification)) + "&nbsp;&nbsp;" + content;
         }
         var label = panelBuilder.newContentPane({
-            content: content
+            content: content,
+            class: "narrafirma-question-header"
         });
         label.placeAt(contentPane);
         return label;
