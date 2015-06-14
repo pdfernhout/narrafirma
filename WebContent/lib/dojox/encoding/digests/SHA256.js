831d8fcaa597cad5d80bd95b98d3759e8384de75
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-06-14 10:20:59 -0400

    Replaced lib/dojo and lib/dojox files with source verstion from Dojo 1.10.4

--- WebContent/lib/dojox/encoding/digests/SHA256.js
+++ WebContent/lib/dojox/encoding/digests/SHA256.js
@@ -1,3 +1,9 @@
-//>>built
-define("dojox/encoding/digests/SHA256",["./_sha-32","./_sha2"],function(a,b){return b(a,256,512,[1779033703,3144134277,1013904242,2773480762,1359893119,2600822924,528734635,1541459225])});
-//# sourceMappingURL=SHA256.js.map
+define(["./_sha-32", "./_sha2"], function(sha32, sha2){
+	//	The 256-bit implementation of SHA-2
+	var hash = [
+		0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+		0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+	];
+
+	return sha2(sha32, 256, 512, hash);
+});
