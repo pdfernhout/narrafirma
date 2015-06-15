0ccae1db6b66db2d3720cd7f5192508c7f0501e3
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-06-15 16:22:13 -0400

    Adding stub typings for dgrid and dstore

---
+++ WebContent/source/typings/dgrid.d.ts
@@ -0,0 +1,35 @@
+// Stub dgrid module by Paul Fernhout
+declare module dgrid {
+    export var Keyboard: any;
+}
+
+declare module "dgrid" {
+    export = dgrid;
+}
+
+declare module "dgrid/Keyboard" {
+    export = dgrid;
+}
+
+declare module "dgrid/extensions/ColumnResizer" {
+    export = dgrid;
+}
+
+declare module "dgrid/extensions/DijitRegistry" {
+    export = dgrid;
+}
+
+declare module "dgrid/Grid" {
+    export = dgrid;
+}
+
+declare module "dgrid/OnDemandGrid" {
+    export = dgrid;
+}
+
+declare module "dgrid/Selection" {
+    export = dgrid;
+}
+
+
+
---
+++ WebContent/source/typings/dstore.d.ts
@@ -0,0 +1,17 @@
+// Stub dgrid module by Paul Fernhout
+declare module dstore {
+    export var Keyboard: any;
+}
+
+declare module "dstore" {
+    export = dstore;
+}
+
+declare module "dstore/Memory" {
+    export = dstore;
+}
+
+declare module "dstore/Trackable" {
+    export = dstore;
+}
+
