07c7379880226d557a16462f9b0aedf56fc08937
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-06-16 10:42:12 -0400

    Replacing javascript and map files under WebContent/js with a README-js.txt file saying they will be generated

---
+++ WebContent/js/README-js.txt
@@ -0,0 +1,3 @@
+Generated js and map files will go here based on the TypeScript code under WebContent/source.
+
+This folder should be marked as derived in Eclipse.
--- WebContent/js/kludgeForUseStrict.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports", "./versions"], function (require, exports, versions) {
-    "use strict";
-    return versions;
-});
-//# sourceMappingURL=kludgeForUseStrict.js.map
