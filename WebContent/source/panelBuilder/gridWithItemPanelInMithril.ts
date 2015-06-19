425f1dbe3fb6202f23ca1766c1bff0be6232c35a
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-06-19 05:39:13 -0400

    Make grid code into a Mithril component

--- WebContent/source/panelBuilder/gridWithItemPanelInMithril.ts
+++ WebContent/source/panelBuilder/gridWithItemPanelInMithril.ts
@@ -83,75 +83,95 @@ function sorts(panelBuilder, list) {
     }
 }
 
-export function add_grid(panelBuilder, model, fieldSpecification) {
-    var configuration = {
-        itemPanelID: undefined,
-        itemPanelSpecification: undefined,
-        idProperty: undefined,
-        gridConfiguration: undefined   
-    };
-    
-    var itemPanelID = fieldSpecification.displayConfiguration;
-    if (!_.isString(itemPanelID)) {
-        configuration = fieldSpecification.displayConfiguration;
-        itemPanelID = configuration.itemPanelID;
-    }
-    
-    var itemPanelSpecification = configuration.itemPanelSpecification;
-    if (!itemPanelSpecification) {
-        itemPanelSpecification = panelBuilder.getPanelDefinitionForPanelID(itemPanelID);
-    }
-    
-    if (!itemPanelSpecification) {
-        console.log("Trouble: no itemPanelSpecification for options: ", fieldSpecification);
-    }
-    
-    if (!model) {
-        console.log("Error: no model is defined for grid", fieldSpecification);
-        throw new Error("Error: no model is defined for grid");
-    }
-    if (!model.get) {
-        console.log("Error: model that supports get is needed for grid", fieldSpecification);
-        throw new Error("Error: model that supports get is needed for grid");
-    }
-    
-    // TODO: May want to use at or similar to get the value in case this is a plain object?
-    var data = model.get(fieldSpecification.id);
-    if (!data) {
-        data = [];
-        model.set(fieldSpecification.id, data);
-    }
-    
-    var idProperty = configuration.idProperty;
-    if (!idProperty) idProperty = "_id";
-
-    // TODO: Issue that this list is regenerated with every draw, and so the sorting is lost!!!
-    var bigData = [];
-    for (var i = 0; i < 5; i++) {
-        for (var j = 0; j < data.length; j++) {
-            var newItem = JSON.parse(JSON.stringify(data[j]));
-            newItem[idProperty] = "item_" + (i * data.length + j);
-            bigData.push(newItem);
-            console.log("newItem", newItem);
+// Grid needs to be a component so it can maintain a local sorted list
+var Grid = {
+    controller: function(args) {
+        var panelBuilder = args.panelBuilder;
+        var model = args.model;
+        var fieldSpecification = args.fieldSpecification;
+        
+        var configuration = {
+            itemPanelID: undefined,
+            itemPanelSpecification: undefined,
+            idProperty: undefined,
+            gridConfiguration: undefined   
+        };
+        
+        var itemPanelID = fieldSpecification.displayConfiguration;
+        if (!_.isString(itemPanelID)) {
+            configuration = fieldSpecification.displayConfiguration;
+            itemPanelID = configuration.itemPanelID;
         }
+        
+        var itemPanelSpecification = configuration.itemPanelSpecification;
+        if (!itemPanelSpecification) {
+            itemPanelSpecification = panelBuilder.getPanelDefinitionForPanelID(itemPanelID);
+        }
+        
+        if (!itemPanelSpecification) {
+            console.log("Trouble: no itemPanelSpecification for options: ", fieldSpecification);
+        }
+        
+        if (!model) {
+            console.log("Error: no model is defined for grid", fieldSpecification);
+            throw new Error("Error: no model is defined for grid");
+        }
+        if (!model.get) {
+            console.log("Error: model that supports get is needed for grid", fieldSpecification);
+            throw new Error("Error: model that supports get is needed for grid");
+        }
+        
+        // TODO: May want to use at or similar to get the value in case this is a plain object?
+        var data = model.get(fieldSpecification.id);
+        if (!data) {
+            data = [];
+            model.set(fieldSpecification.id, data);
+        }
+        
+        var idProperty = configuration.idProperty;
+        if (!idProperty) idProperty = "_id";
+        this.idProperty = idProperty;
+        
+        var bigData = [];
+        for (var i = 0; i < 5; i++) {
+            for (var j = 0; j < data.length; j++) {
+                var newItem = JSON.parse(JSON.stringify(data[j]));
+                newItem[idProperty] = "item_" + (i * data.length + j);
+                bigData.push(newItem);
+                console.log("newItem", newItem);
+            }
+        }
+        data = bigData;
+        
+        var columns = computeColumnsForItemPanelSpecification(itemPanelSpecification, configuration);
+     
+        this.data = data;
+        this.columns = columns;
+    },
+    
+    view: function(ctrl, args) {
+        var panelBuilder = args.panelBuilder;
+        
+        // return m("table", sorts(ctrl.list), [
+        var table = m("table", sorts(panelBuilder, ctrl.data), [
+            m("tr[style=outline: thin solid; background-color: #66CCFF]", ctrl.columns.map(function (column) {
+                    return m("th[data-sort-by=" + column.field  + "]", {"text-overflow": "ellipsis"}, column.label)
+                }).concat(m("th", ""))
+            ),
+            ctrl.data.map(function(item) {
+                return m("tr", {key: item[ctrl.idProperty]}, ctrl.columns.map(function (column) {
+                    return m("td[style=outline: thin solid]", {"text-overflow": "ellipsis"}, item[column.field])
+                }).concat(m("td[style=outline: thin solid]", {nowrap: true}, [m("button", "delete"), m("button", "edit"), m("button", "view")])))
+            })
+        ]);
+        
+        // TODO: set class etc.
+        return m("div", [table]);
     }
-    data = bigData;
+};
     
-    var columns = computeColumnsForItemPanelSpecification(itemPanelSpecification, configuration);
-    
-    // return m("table", sorts(ctrl.list), [
-    var table = m("table", sorts(panelBuilder, data), [
-        m("tr[style=outline: thin solid; background-color: #66CCFF]", columns.map(function (column) {
-                return m("th[data-sort-by=" + column.field  + "]", {"text-overflow": "ellipsis"}, column.label)
-            }).concat(m("th", ""))
-        ),
-        data.map(function(item) {
-            return m("tr", {key: item[idProperty]}, columns.map(function (column) {
-                return m("td[style=outline: thin solid]", {"text-overflow": "ellipsis"}, item[column.field])
-            }).concat(m("td[style=outline: thin solid]", {nowrap: true}, [m("button", "delete"), m("button", "edit"), m("button", "view")])))
-        })
-    ]);
-    
-    // TODO: set class etc.
-    return m("div", [table]);
+
+export function add_grid(panelBuilder, model, fieldSpecification) {
+
+    return m.component(<any>Grid, {panelBuilder: panelBuilder, model: model, fieldSpecification: fieldSpecification});
 }
