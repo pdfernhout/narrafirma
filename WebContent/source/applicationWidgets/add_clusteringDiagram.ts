367a79c4b127f5ca74b9431d5705e55b7e6be1b6
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-06-19 12:34:16 -0400

    Stub out clustering diagram for Mithril so at least displays without exception

--- WebContent/source/applicationWidgets/add_clusteringDiagram.ts
+++ WebContent/source/applicationWidgets/add_clusteringDiagram.ts
@@ -1,19 +1,21 @@
 import ClusteringDiagram = require("./ClusteringDiagram");
 import PanelBuilder = require("../panelBuilder/PanelBuilder");
+import m = require("mithril");
 
 "use strict";
 
-function add_clusteringDiagram(panelBuilder: PanelBuilder, contentPane, model, fieldSpecification) {
+function add_clusteringDiagram(panelBuilder: PanelBuilder, model, fieldSpecification) {
     // clustering diagram using a list of 2D objects
     console.log("add_clusteringDiagram", model, fieldSpecification);
     // console.log("clusteringDiagram module", clusteringDiagram);
     
     var diagramName = fieldSpecification.displayConfiguration;
     
-    var questionContentPane = panelBuilder.createQuestionContentPaneWithPrompt(contentPane, fieldSpecification);
+    // var questionContentPane = panelBuilder.createQuestionContentPaneWithPrompt(contentPane, fieldSpecification);
     
-    var clusteringDiagram = new ClusteringDiagram(questionContentPane, model, fieldSpecification.id, diagramName, true);
+    // var clusteringDiagram = new ClusteringDiagram(questionContentPane, model, fieldSpecification.id, diagramName, true);
     
+    /* TODO: Who should be responsible for updating this data? Is redraw called or is that bypassed as an html component?
     var watcher = model.watch(diagramName, function() {
         // console.log("updating clusteringDiagram for model field change", model, fieldSpecification);
         var newData = model.get(diagramName);
@@ -23,8 +25,11 @@ function add_clusteringDiagram(panelBuilder: PanelBuilder, contentPane, model, f
     // Klugde to get the contentPane to free the watcher by calling remove when it is destroyed
     // This would not work if the content pane continued to exist when replacing this component
     contentPane.own(watcher);
+    */
     
-    return clusteringDiagram;
+    // return clusteringDiagram;
+    
+    return m("div", "Put clustering diagram here");
 }
 
 export = add_clusteringDiagram;
