68ac247e05dc522190fe2032e44fc8ed07aa5f6c
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-06-24 08:30:17 -0400

    Get add_clusteringDiagram workign with Mithril
    Still may need more work on dealing with domain model updates?

--- WebContent/source/applicationWidgets/add_clusteringDiagram.ts
+++ WebContent/source/applicationWidgets/add_clusteringDiagram.ts
@@ -7,14 +7,13 @@ import m = require("mithril");
 function add_clusteringDiagram(panelBuilder: PanelBuilder, model, fieldSpecification) {
     // clustering diagram using a list of 2D objects
     console.log("add_clusteringDiagram", model, fieldSpecification);
-    // console.log("clusteringDiagram module", clusteringDiagram);
     
     var diagramName = fieldSpecification.displayConfiguration;
     
-    // var questionContentPane = panelBuilder.createQuestionContentPaneWithPrompt(contentPane, fieldSpecification);
-    
-    // var clusteringDiagram = new ClusteringDiagram(questionContentPane, model, fieldSpecification.id, diagramName, true);
-    
+    var prompt = panelBuilder.buildQuestionLabel(fieldSpecification);
+        
+    var clusteringDiagram = m.component(<any>ClusteringDiagram, {model: model, id: fieldSpecification.id, diagramName: diagramName, autosave: true});
+
     /* TODO: Who should be responsible for updating this data? Is redraw called or is that bypassed as an html component?
     var watcher = model.watch(diagramName, function() {
         // console.log("updating clusteringDiagram for model field change", model, fieldSpecification);
@@ -27,9 +26,10 @@ function add_clusteringDiagram(panelBuilder: PanelBuilder, model, fieldSpecifica
     contentPane.own(watcher);
     */
     
-    // return clusteringDiagram;
-    
-    return m("div", "Put clustering diagram here");
+    return m("div", [
+        prompt,
+        clusteringDiagram
+    ]);
 }
 
 export = add_clusteringDiagram;
