7fc5cb45e8231460cdf06a931faa572db31cc309
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-06-26 13:46:50 -0400

    Have ListChooser use regular dialog to open it

--- WebContent/source/panelBuilder/dialogSupport.ts
+++ WebContent/source/panelBuilder/dialogSupport.ts
@@ -60,17 +60,21 @@ class MithrilDialog {
             internalView = m("div", "Problem creating view");
         }
         
-        return m("div.overlay", m("div.modal-content", [
+        var parts = [
             m("b", translate(dialogConfiguration.dialogTitle)),
-            m("div.modal-internal", internalView),
-            m("button", {onclick: function() {
+            m("div.modal-internal", internalView)
+        ];
+        
+        if (!dialogConfiguration.dialogOKButtonHidden) {
+            parts.push(m("button", {onclick: function() {
                 if (dialogConfiguration.dialogOKCallback) {
                     dialogConfiguration.dialogOKCallback(dialogConfiguration, hideDialogMethod);
                 } else {
                     hideDialogMethod();
                 }
-            }}, translate(args.dialogOKButtonLabel || "OK"))
-        ]));
+            }}, translate(args.dialogOKButtonLabel || "OK")));
+        }
+        return m("div.overlay", m("div.modal-content", parts));
     }
 }
 
@@ -111,57 +115,45 @@ function build_textEditorDialogContent(dialogConfiguration, hideDialogMethod) {
     ]);
 }
 
-// TODO: Have ListChooser use regular dialog to open it
-
-class ListChooser {
-    static controller() {
-        console.log("Making ListChooser");
-        return new ListChooser();
-    }
-    
-    static view(controller) {
-        console.log("ListChooser view called");
-        
-        return controller.calculateView(globalDialogConfiguration);
-    }
-    
-    calculateView(args) {
-        return m("div.overlay", m("div.modal-content", [
-            m("b", args.dialogTitle),
-            m("br"),
-            args.dialogOKButtonLabel,
-            m("br"),
-            args.choices.map((choice) => {
-                return m("button", {onclick: this.selectionMade.bind(this, args, choice)}, choice.name);
-            })
-        ]));
-    }
-    
-    selectionMade(args, choice) {
-        hideDialogMethod();
-        args.dialogOKCallback(choice);
-    }
-    
-}
-
 // columns are currently ignored
 // choices should be a list of objects with a name field, like: {name: "test", other: "???}
 export function openListChoiceDialog(initialChoice, choices, columns, dialogTitle, dialogOKButtonLabel, dialogOKCallback) {
     if (!dialogTitle) dialogTitle = "Choices";
     if (!dialogOKButtonLabel) dialogOKButtonLabel = "Choose";
     
-    globalDialogConfiguration = {
+    var dialogConfiguration = {
         key: "standardListChooser",
         initialChoice: initialChoice,
         choices: choices,
         columns: columns,
         dialogTitle: dialogTitle,
         dialogOKButtonLabel: dialogOKButtonLabel, 
-        dialogOKCallback: dialogOKCallback
+        dialogOKCallback: dialogOKCallback,
+        dialogConstructionFunction: build_listChooserDialogContent,
+        dialogOKButtonHidden: true
     };
     
-    setTimeout(function() {
-        m.mount(document.getElementById("dialogDiv"), <any>ListChooser);
-    }, 0);
+    openDialog(dialogConfiguration);
 }
+
+function build_listChooserDialogContent(dialogConfiguration, hideDialogMethod) {
+    
+    function selectionMade(args, choice) {
+        hideDialogMethod();
+        args.dialogOKCallback(choice);
+    }
+    
+    // style: "min-height: 400px; min-width: 600px; max-height: 800px; max-width: 800px; overflow: auto"
+    return m("div.overlay", m("div.modal-content", [
+        m("b", dialogConfiguration.dialogTitle),
+        m("br"),
+        dialogConfiguration.dialogOKButtonLabel,
+        m("br"),
+        dialogConfiguration.choices.map((choice) => {
+            return m("button", {onclick: selectionMade.bind(null, dialogConfiguration, choice)}, choice.name);
+        })
+    ]));
+}
+
+
 
