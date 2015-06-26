e8bea499052295305396ad50b033a9bc48c5c850
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-06-26 08:45:41 -0400

    Add TypeScript interfaces for GridConfiguration and GridDisplayConfiguration

--- WebContent/source/narrafirmaTypes.d.ts
+++ WebContent/source/narrafirmaTypes.d.ts
@@ -26,3 +26,28 @@ interface GraphHolder {
     currentGraph: any;
     currentSelectionExtentPercentages: GraphSelection;
 }
+
+// Possible Grid configuration options
+interface GridConfiguration {
+    idProperty?: string;
+    inlineButtons?: boolean;
+    viewButton?: boolean;
+    addButton?: boolean;
+    removeButton?: boolean;
+    editButton?: boolean;
+    duplicateButton?: boolean;
+    moveUpDownButtons?: boolean;
+    navigationButtons?: boolean;
+    // For next field, Array could be ["fieldName1", "fieldName2", ...]
+    includeAllFields?: boolean | Array<String>;
+    customButton?: any;
+    validateAdd?: string;
+    validateEdit?: string;
+    shouldNextItemBeSelectedAfterItemRemoved?: boolean;
+}
+
+interface GridDisplayConfiguration {
+    itemPanelID: string;
+    itemPanelSpecification: any;
+    gridConfiguration: GridConfiguration;
+}
