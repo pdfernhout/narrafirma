1078d6c1541c77144478fe013f80bee239315166
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-07-04 20:27:49 -0400

    Make progress on getting panelWithGridModularSaveLoadTest.js to work with newer verion of Pointrel in loopback mode
    Does not yet support grid objects being stored

--- WebContent/testing/panelWithGridModularSaveLoadTest.js
+++ WebContent/testing/panelWithGridModularSaveLoadTest.js
@@ -1,54 +1,20 @@
 require([
     "js/panelBuilder/PanelBuilder",
     "js/panelBuilder/PanelSpecificationCollection",
-    "js/pointrel20141201Client",
+    "js/pointrel20150417/PointrelClient",
+    "js/pointrel20150417/TripleStore",
     "mithril"
 ], function(
     PanelBuilder,
     PanelSpecificationCollection,
-    pointrel20141201Client,
+    PointrelClient,
+    TripleStore,
     m
 ){
     "use strict";
     
-    console.log("panelWithGridModularSaveLoadTest.js", pointrel20141201Client);
-    
-    function storeModel(model, documentID, previous, committerID, callbackWhenDone) {
-        var contentType = "org.workingwithstories.Test3Model";
-        var contentVersion = "0.1.0";
-        
-        var metadata = {
-            id: documentID,
-            previous: previous,
-            tags: [],
-            contentType: contentType,
-            contentVersion: contentVersion,
-            author: null,
-            committer: committerID,
-            timestamp: true
-        };        
-        pointrel20141201Client.storeInNewEnvelope(model, metadata, function(error, serverResponse) {
-            if (error) {
-                console.log("could not write new model version:\n" + error);
-                return callbackWhenDone(error);
-            }
-            var sha256HashAndLength = serverResponse.sha256AndLength;
-            console.log("wrote sha256HashAndLength:", sha256HashAndLength);
-            callbackWhenDone(null, sha256HashAndLength);
-        });
-    }
-    
-    function loadLatestModelVersion(documentID, callbackWhenDone) {
-        console.log("============= loadLatestModelVersion");
-        pointrel20141201Client.loadLatestEnvelopeForID(documentID, function(error, envelope) {
-            if (error) {
-                if (error === "No items found for id") error = "No stored versions could be loaded -- have any versions been saved?";
-                return callbackWhenDone(error);
-            }
-            callbackWhenDone(null, envelope.content, envelope);           
-        });
-    }
-    
+    console.log("panelWithGridModularSaveLoadTest.js");
+
     var page_partipantGroupsPanelSpecification = {
         "id": "page_participantGroups",
         "displayName": "Describe participant groups",
@@ -101,6 +67,14 @@ require([
     };
     
     function test() {
+        // Thinking project ID should be a UUID?
+        var projectID = "TestProject1234";
+        var documentID = projectID + "-participantGroups";
+        var committerID = "tester@example.com";
+        
+        var pointrelClient = new PointrelClient("loopback", projectID, committerID);
+        var tripleStore = new TripleStore(pointrelClient, documentID);
+     
         var panels = new PanelSpecificationCollection();
         
         panels.addPanelSpecification(page_partipantGroupsPanelSpecification);
@@ -109,78 +83,18 @@ require([
         
         var testModelTemplate = panels.buildModel("Test3Model");
         
-        // var testModel = "Test3";
-        var testModel = {};
-        
-        // STOPPED HERE!!! Would need to implement additional support in PanelSpecificationCollection and add_grid for a "store" type in addition to current "array"
-        // testModel.set("project_participantGroupsList", {type: "store", itemClass: "ParticipantGroupModel"});
+        var testModel = "Test3";
+        // var testModel = {};
         
         console.log("testModel", testModel);
         
         var panelBuilder = new PanelBuilder({panelSpecificationCollection: panels});
+        panelBuilder.project = {tripleStore: tripleStore};
         
         function testView() {
-        
-            var page = panelBuilder.buildPanel("page_participantGroups", testModel);
-            
-            var loadLatestButtonSpecification = {
-                id: "loadButton",
-                displayType: "button",
-                displayPrompt: "Load latest",
-                displayConfiguration: loadLastestModelVersion,
-                displayPreventBreak: true
-            };
-            
-            var loadLatestButton = panelBuilder.buildField(testModel, loadLatestButtonSpecification);
-            
-            var storeButtonSpecification = {
-                id: "storeButton",
-                displayType: "button",
-                displayPrompt: "Store",
-                displayConfiguration: storeModelVersion
-            };
-            
-            var storeButton = panelBuilder.buildField(testModel, storeButtonSpecification);
-            
-            return [page, loadLatestButton, storeButton];
-        }
-        
-        // Thinking project ID should be a UUID?
-        var projectID = "TestProject1234";
-        var documentID = projectID + "-participantGroups";
-        var committerID = "tester@example.com";
-        
-        function loadLastestModelVersion() {
-            console.log("trying to load latest");
-            loadLatestModelVersion(documentID, function(error, latestModelVersion, envelope) {
-                console.log("load result", error, latestModelVersion, envelope);
-                if (error) {
-                    alert("An error happened when loading: " + error);
-                } else {
-                    console.log("Loaded OK");
-                    for (var key in testModelTemplate) {
-                        console.log("key", key);
-                        if (testModel.hasOwnProperty(key)) {
-                            console.log("Trying to copy", key);
-                            testModel.set(key, latestModelVersion[key]);
-                        }
-                    }
-                    alert("Loaded OK\n" + envelope.__sha256HashAndLength);
-                }
-            });
-        }
-        
-        function storeModelVersion() {
-            console.log("trying to store", testModel);
-            storeModel(testModel, documentID, null, committerID, function(error, sha256AndLength) {
-                console.log("store result", error, sha256AndLength);
-                if (error) {
-                    alert("An error happened when storing: " + error);
-                } else {
-                    alert("Stored OK\n" + sha256AndLength);
-                }
-            });
-        }
+            console.log("view called", tripleStore);
+            return panelBuilder.buildPanel("page_participantGroups", testModel);
+        }   
         
         m.mount(document.getElementById("pageDiv"), {view: testView});
     }
