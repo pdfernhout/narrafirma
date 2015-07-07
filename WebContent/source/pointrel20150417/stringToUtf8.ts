86ac29f28a640d6f73ad900611a5a30d536a371b
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-07-07 18:42:20 -0400

    Simplify stringToUtf8

--- WebContent/source/pointrel20150417/stringToUtf8.ts
+++ WebContent/source/pointrel20150417/stringToUtf8.ts
@@ -1,33 +1,13 @@
 import kludgeForUseStrict = require("../kludgeForUseStrict");
 
-// X stringToUtf8 function from Dojo dojox/encoding/digests/_base.js -- convert a JavaScript string to UTF-8
-
 "use strict";
 /* tslint:disable */
 
-function stringToUtf8(input){
-    var output = "";
-    var i = -1;
-    var x, y;
+// Using deprecated funciton
+declare var unescape;
 
-    while(++i < input.length){
-        x = input.charCodeAt(i);
-        y = i + 1 < input.length ? input.charCodeAt(i + 1) : 0;
-        if(0xD800 <= x && x <= 0xDBFF && 0xDC00 <= y && y <= 0xDFFF){
-            x = 0x10000 + ((x & 0x03FF) << 10) + (y & 0x03FF);
-            i++;
-        }
-
-        if(x <= 0x7F)
-            output += String.fromCharCode(x);
-        else if(x <= 0x7FF)
-            output += String.fromCharCode(0xC0 | ((x >>> 6) & 0x1F), 0x80 | (x & 0x3F));
-        else if(x <= 0xFFFF)
-            output += String.fromCharCode(0xE0 | ((x >>> 12) & 0x0F), 0x80 | ((x >>> 6) & 0x3F), 0x80 | (x & 0x3F));
-        else if(x <= 0x1FFFFF)
-            output += String.fromCharCode(0xF0 | ((x >>> 18) & 0x07), 0x80 | ((x >>> 12) & 0x3F), 0x80 | ((x >>> 6) & 0x3F), 0x80 | (x & 0x3F));
-    }
-    return output;
-}
-
+// From http://stackoverflow.com/questions/18729405/how-to-convert-utf8-string-to-byte-array
+function stringToUtf8(input): string {
+    return unescape(encodeURIComponent(input));
+};
 export = stringToUtf8;
