5c49735d1546573230e7a710a21296f91872d57a
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-07-10 19:07:47 -0400

    Make progress on displaying story collection status as main dashboard widget

--- WebContent/source/applicationWidgets/add_dashboardStoryCollectionStatusDisplay.ts
+++ WebContent/source/applicationWidgets/add_dashboardStoryCollectionStatusDisplay.ts
@@ -1,12 +1,37 @@
 import translate = require("../panelBuilder/translate");
 import PanelBuilder = require("../panelBuilder/PanelBuilder");
+import surveyCollection = require("../surveyCollection");
 import Project = require("../Project");
 import m = require("mithril");
 
 "use strict";
 
 function add_dashboardStoryCollectionStatusDisplay(panelBuilder: PanelBuilder, model, fieldSpecification): any {
-    return m("div", ["add_dashboardStoryCollectionStatusDisplay unfinished"]);
+    var tripleStore = panelBuilder.project.tripleStore;
+    var storyCollections = panelBuilder.project.getListForField("project_storyCollections");
+    if (!storyCollections || !storyCollections.length) {
+        // TODO: Translate
+        return m("div", ["No story collections defined"]);
+    }
+    
+    return m("div.narrafirma-dashboard-story-collection-status", [
+        m("i", "Story collections:"),
+        m("br"),
+        m("table", 
+            m("tr", [m("th", "Name"), m("th", "Count"), m("th", "Active?")]),
+            storyCollections.map(function(storyCollectionIdentifier) {
+            var shortName = tripleStore.queryLatestC(storyCollectionIdentifier, "storyCollection_shortName");
+            var allStoriesInStoryCollection = surveyCollection.getStoriesForStoryCollection(shortName);
+            var storyCount = allStoriesInStoryCollection.length;
+            var activeOnWeb = tripleStore.queryLatestC(storyCollectionIdentifier, "storyCollection_activeOnWeb");
+            var surveyActive = activeOnWeb ? m("a", {href: activeOnWeb, target: "_blank"}, "active") : "";
+            return m("tr", [
+                m("td", shortName),
+                m("td", {style: "text-align: right;"}, storyCount),
+                m("td", surveyActive),
+            ]);
+        }))
+    ]);
 
 }
 
