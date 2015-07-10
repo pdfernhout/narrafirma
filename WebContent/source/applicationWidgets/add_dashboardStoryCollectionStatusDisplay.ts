bfdc15887c436997dfb72c3e34ca1f88095cf75a
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-07-10 19:16:17 -0400

    Sort story collection list by shortName in story collection status dashboard widget

--- WebContent/source/applicationWidgets/add_dashboardStoryCollectionStatusDisplay.ts
+++ WebContent/source/applicationWidgets/add_dashboardStoryCollectionStatusDisplay.ts
@@ -8,29 +8,44 @@ import m = require("mithril");
 
 function add_dashboardStoryCollectionStatusDisplay(panelBuilder: PanelBuilder, model, fieldSpecification): any {
     var tripleStore = panelBuilder.project.tripleStore;
-    var storyCollections = panelBuilder.project.getListForField("project_storyCollections");
-    if (!storyCollections || !storyCollections.length) {
+    var storyCollectionsIdentifiers = panelBuilder.project.getListForField("project_storyCollections");
+    if (!storyCollectionsIdentifiers || !storyCollectionsIdentifiers.length) {
         // TODO: Translate
         return m("div", ["No story collections defined"]);
     }
+    
+    var storyCollections = storyCollectionsIdentifiers.map(function(storyCollectionIdentifier) {
+        var shortName = tripleStore.queryLatestC(storyCollectionIdentifier, "storyCollection_shortName");
+        var allStoriesInStoryCollection = surveyCollection.getStoriesForStoryCollection(shortName);
+        var storyCount = allStoriesInStoryCollection.length;
+        var activeOnWeb = tripleStore.queryLatestC(storyCollectionIdentifier, "storyCollection_activeOnWeb");
+        var surveyActive = activeOnWeb ? m("a", {href: activeOnWeb, target: "_blank"}, "active") : "";
+        return {
+            id: storyCollectionIdentifier,
+            shortName: shortName,
+            storyCount: storyCount,
+            activeOnWeb: activeOnWeb
+        };
+    });
+    
+    storyCollections.sort(function(a, b) {
+        return a.shortName.localeCompare(b.shortName);
+    });
     
     return m("div.narrafirma-dashboard-story-collection-status", [
         m("i", "Story collections:"),
         m("br"),
         m("table", 
             m("tr", [m("th", "Name"), m("th", "Count"), m("th", "Active?")]),
-            storyCollections.map(function(storyCollectionIdentifier) {
-            var shortName = tripleStore.queryLatestC(storyCollectionIdentifier, "storyCollection_shortName");
-            var allStoriesInStoryCollection = surveyCollection.getStoriesForStoryCollection(shortName);
-            var storyCount = allStoriesInStoryCollection.length;
-            var activeOnWeb = tripleStore.queryLatestC(storyCollectionIdentifier, "storyCollection_activeOnWeb");
-            var surveyActive = activeOnWeb ? m("a", {href: activeOnWeb, target: "_blank"}, "active") : "";
-            return m("tr", [
-                m("td", shortName),
-                m("td", {style: "text-align: right;"}, storyCount),
-                m("td", surveyActive),
-            ]);
-        }))
+            storyCollections.map(function(storyCollection) {
+                var surveyActive = storyCollection.activeOnWeb ? m("a", {href: storyCollection.activeOnWeb, target: "_blank"}, "active") : "";
+                return m("tr", [
+                    m("td", storyCollection.shortName),
+                    m("td", {style: "text-align: right;"}, storyCollection.storyCount),
+                    m("td", surveyActive),
+                ]);
+            })
+        )
     ]);
 
 }
