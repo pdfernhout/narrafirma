c416dab3f11f2afd8c0c0e80f32bcd1b0df8d201
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-07-13 10:10:30 -0400

    Get storiesList widget to work for project stores
    Stills needs to be generalized later to handle other story lists.

--- WebContent/source/applicationWidgets/add_storiesList.ts
+++ WebContent/source/applicationWidgets/add_storiesList.ts
@@ -5,19 +5,39 @@ import m = require("mithril");
 "use strict";
 
 function add_storiesList(panelBuilder: PanelBuilder, model, fieldSpecification) {
-    return m("div", "add_storiesList UNFINISHED");
-    
-    // TODO: Fix for Mithril
-    /*
-    var questionContentPane = panelBuilder.createQuestionContentPaneWithPrompt(fieldSpecification);
-    
-    var label = panelBuilder.newContentPane({
-        // content: translate(id + "::prompt", fieldSpecification.displayPrompt)
-        content: "<b>UNFINISHED add_storiesList: " + fieldSpecification.id + "</b>"             
+    var prompt = panelBuilder.buildQuestionLabel(fieldSpecification);
+
+    var tripleStore = panelBuilder.project.tripleStore;
+
+    // TODO: Generalize this so it can be abut more than just project stories
+    var storyIdentifierList = panelBuilder.project.getListForField("project_projectStoriesList");
+    var storyNameField = "projectStory_name";
+    var storyTextField = "projectStory_text";
+   
+    storyIdentifierList.sort(function(a, b) {
+        var aName = tripleStore.queryLatestC(a, storyNameField) || "";
+        var bName = tripleStore.queryLatestC(b, storyNameField) || "";
+        return aName.localeCompare(bName);
     });
-    label.placeAt(questionContentPane);
-    return label;
-    */
+    
+    return m("div.narrafirma-stories-list", [
+        prompt,
+        m("i", "Project stories:"),
+        m("br"),
+        m("table", [
+            m("tr", [m("th", "Story name"), m("th", "Story text")]),
+            storyIdentifierList.map((storyIdentifier) => {
+                console.log("storyIdentifier", storyIdentifier);
+                return [
+                    m("td", tripleStore.queryLatestC(storyIdentifier, storyNameField)),
+                    m("td", tripleStore.queryLatestC(storyIdentifier, storyTextField))
+                ];
+            })
+        ])
+    ]);
 }
 
 export = add_storiesList;
+
+
+
