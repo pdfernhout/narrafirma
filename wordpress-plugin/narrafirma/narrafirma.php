6586d55341bdf2bff00f20ab549783eb5b52f565
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-07-29 13:49:43 -0400

    Update narrafirma.php plugin to match current index.html for Mithril

--- wordpress-plugin/narrafirma/narrafirma.php
+++ wordpress-plugin/narrafirma/narrafirma.php
@@ -3,7 +3,7 @@
 Plugin Name: NarraFirma
 Description: Grounding your action on stories
 Author: Cynthia F. Kurtz and Paul D. Fernhout
-Version: 0.1
+Version: 0.2
 */
 
 function create_my_plugin_options_panel() {
@@ -11,53 +11,65 @@ function create_my_plugin_options_panel() {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
   $baseDirectory = plugins_url( 'narrafirma' );
+  
+  add_my_scripts();
+    
   echo <<<ENDSCRIPT
-<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/resources/dojo.css">
-<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dijit/themes/dijit.css">
-<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dijit/themes/claro/claro.css">
-<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojox/layout/resources/ResizeHandle.css">
-<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojox/widget/Wizard/Wizard.css">
-<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojox/widget/Toaster/Toaster.css">
-
-<link rel="stylesheet" href="$baseDirectory/lib/dgrid/css/dgrid.css">
-<link rel="stylesheet" href="$baseDirectory/lib/dgrid/css/skins/claro.css">
-<link rel="stylesheet" href="$baseDirectory/lib/dgrid/css/extensions/ColumnResizer.css">
-
+<link rel="stylesheet" href="$baseDirectory/lib/humane/original.css">
+<link rel="stylesheet" href="$baseDirectory/css/survey.css">
 <link rel="stylesheet" href="$baseDirectory/css/standard.css">
 
-  <script type='text/javascript'>
-    var dojoConfig;
-    (function() {
-        // Determine the containing location of this page by removing the actual page name (e.g. "test.html") at the end
-        var baseDirectoryFromURL = "$baseDirectory";
-        dojoConfig = {
-            async: true,
-            parseOnLoad: false,
-            // cacheBust: "1.10.4",
-            // Load dgrid and its dependencies from a local copy.
-            // More complexity is required because these are not in the same directory as dojo modules when loaded locally or dojo base is loaded from CDN
-            packages: [
-                { name: "js", location: baseDirectoryFromURL + "/js"},
-                { name: "lib", location: baseDirectoryFromURL + "/lib"},
-                { name: "dgrid", location: baseDirectoryFromURL + "/lib/dgrid" },
-                { name: "dstore", location: baseDirectoryFromURL + "/lib/dstore" },
-                { name: "put-selector", location: baseDirectoryFromURL + "/lib/put-selector" },
-                { name: "xstyle", location: baseDirectoryFromURL + "/lib/xstyle" }
-            ]
-        };
-    }());
-  </script>
-  
-  <div class="claro">
-    <noscript>Please enable JavaScript to use NarraFirma. NarraFirma is companion software to the book Working With Stories in Your Community or Organization.</noscript>
-    <div id="pleaseWaitDiv" style="display: none"><b>Starting up NarraFirma; please wait...</b></div>
-    <script>console.log("started loading..."); document.getElementById("pleaseWaitDiv").style.display="block";</script>
-        
-    <div id="navigationDiv"></div>
-    <div id="pageDiv" style="overflow: auto"></div>
-  </div>
+<script>
+var require = {
+    baseUrl: '$baseDirectory',
+    paths: {
+        js: "js",
+        applicationPanelSpecifications: "applicationPanelSpecifications",
+        mithril: "lib/mithril/mithril",
+        d3: "lib/d3/d3",
+        humane: "lib/humane/humane",
+        lib: "lib"
+    }
+};
+</script>
+
+<script type='text/javascript' src="$baseDirectory/lib/lodash.js"></script>
+<script type='text/javascript' src="$baseDirectory/lib/fileSaver/Blob.js"></script>
+<script type='text/javascript' src="$baseDirectory/lib/fileSaver/FileSaver.js"></script>
+<script src="$baseDirectory/lib/require.min.js"></script>
+
+<noscript>Please enable JavaScript to use NarraFirma. NarraFirma is companion software to the book Working With Stories in Your Community or Organization.</noscript>
+<div id="pleaseWaitDiv" class="pleaseWaitOverlay" style="display: none">Starting up NarraFirma; please wait...</div>
+<script>console.log("started loading..."); document.getElementById("pleaseWaitDiv").style.display="block";</script>
+
+<div id="navigationDiv" style="display: none"></div>
+<div id="pageDiv" style="overflow: auto; display: none;"></div>
+<div id="toasterDiv"></div>
+<div id="dialogDiv"></div>
+<script>
+// Load text with plugins here because TypeScript IDE does not like it elsehwere
+require([
+    "js/main",
+    "lib/text!applicationPanelSpecifications/navigation.json",
+    "lib/text!recommendations/recommendations_filledin.csv",
+    "lib/text!recommendations/recommendations_intervention_filledin.csv"
+], function(
+    main,
+    navigationJSONText,
+    recommendationsText,
+    recommendationsInterventionText
+) {
+    // Load these here because TypeScript does not like the plugin use
+    window["narraFirma_navigationJSONText"] = navigationJSONText;
+    window["narraFirma_recommendationsText"] = recommendationsText;
+    window["narraFirma_recommendationsInterventionText"] = recommendationsInterventionText;
+    
+    console.log("Launching main application");
+    main.run();
+});
+</script>
+
 ENDSCRIPT;
-  add_my_scripts();
 }
 
 function add_my_plugin_menu() {
@@ -65,9 +77,8 @@ function add_my_plugin_menu() {
 }
 
 function add_my_scripts() {
-  wp_enqueue_script( 'pointrel-lodash', plugins_url( 'narrafirma/lib/lodash.js'), array(), '1.0.0', true );
-  wp_enqueue_script( 'pointrel-dojo', '//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js', array(), '1.0.0', true );
-  wp_enqueue_script( 'pointrel-main', plugins_url( 'narrafirma/js/main.js'), array(), '1.0.0', true );
+  // wp_enqueue_script( 'pointrel-lodash', plugins_url( 'narrafirma/lib/lodash.js'), array(), '1.0.0', true );
+  // wp_enqueue_script( 'pointrel-main', plugins_url( 'narrafirma/js/main.js'), array(), '1.0.0', true );
 }
 
 // add_action( 'wp_enqueue_scripts', 'add_my_scripts' );
