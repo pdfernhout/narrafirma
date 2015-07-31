417fa3a78eb737d51a3e47ae6a47b4007ac25df5
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-07-31 09:42:26 -0400

    Add test of defining and calling JavaScript for a canvas element in WordPress plugin
    Just learning about how this might work...

--- WebContent/narrafirma.php
+++ WebContent/narrafirma.php
@@ -200,6 +200,16 @@ function narrafirma_create_options_panel() {
 Options relating to the NarraFirma Plugin.
 </div>
 
+<canvas id="myCanvas" width="200" height="100" style="border:1px solid #000000;">
+</canvas> 
+<script>
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,0,150,75);
+</script>
+
+
 <?php
 }
 
