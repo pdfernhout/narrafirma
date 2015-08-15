f25c2a7bc39d73f719f289a1d8ba7c7a118d5c86
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-08-15 17:39:11 -0400

    Add support to cancel changes in WordPress plugin admin
    Also displayign more content about journal permissions

--- wordpress-plugin/narrafirma/narrafirmaWordpressAdmin.js
+++ wordpress-plugin/narrafirma/narrafirmaWordpressAdmin.js
@@ -1,5 +1,7 @@
 (function() {
     "use strict";
+    
+    // Surprising issue: If you reload this page after making changes, the changes will reappear in Firefox because the textarea's content is carried forward.
 
     console.log("narrafirmaWordpressAdmin called");
     
@@ -16,20 +18,70 @@
             return {
                 greeting: "Hello from NarraFirmaAdminComponent",
                 showJSON: false,
-                journalDefinitions: readJournalDefinitions()
+                // Read JSON twice to ensure changing the second copy won't affect the first
+                originalJournalDefinitions: readJournalDefinitionsFromTextarea(),
+                journalDefinitions: readJournalDefinitionsFromTextarea()
             };
         },
         view: function(controller) {
+            var isJSONUnchanged = true;
+            try {
+                isJSONUnchanged = JSON.stringify(controller.originalJournalDefinitions) === JSON.stringify(controller.journalDefinitions);
+            } catch (e) {
+                console.log("Problem comparing JSON for old and new journal definitions", e);
+            }
+                
             return m("div", [
                 m("h1", controller.greeting),
                 Object.keys(controller.journalDefinitions).map(function(key) {
-                    return m("div", key);
+                    var journal = controller.journalDefinitions[key];
+                    return m("div", [
+                        key,
+                        m("button", {onclick: deleteJournal.bind(null, controller, key)}, "delete"),
+                        m("br"),
+                        m("div", "  write: " + journal.write),
+                        m("div", "  read: " + journal.read),
+                        m("div", "  survey: " + journal.survey),
+                        m("div", "  anonymous survey: " + (journal.survey.indexOf(true) !== -1)),
+                    ]);
                 }),
+                m("button", {onclick: newProject.bind(null, controller)}, "New project"),
+                m("br"),
+                m("button", {onclick: cancelChanges.bind(null, controller), disabled: isJSONUnchanged}, "Cancel changes"),
+                m("hr"),
                 m("span", {"for": "narrafirma-displayJSON"}, "Edit JSON directly"),
                 m("input[type=checkbox]", {id: "narrafirma-displayJSON", onclick: m.withAttr("checked", showJSONChecked.bind(null, controller)), checked: controller.showJSON})
             ]);
         }
     };
+    
+    function deleteJournal(controller, key) {
+        if (!confirm("Are you sure you want to delete: " + key + "?")) return;
+        delete controller.journalDefinitions[key];
+        writeJournalDefinitionsToTextarea(controller.journalDefinitions);
+    }
+    
+    function newProject(controller) {
+        var newName = prompt("New project name?");
+        if (!newName) return;
+        var key = "NarraFirmaProject-" + newName;
+        if (controller.journalDefinitions[key]) {
+            alert("A project with that name already exists");
+            return;
+        }
+        controller.journalDefinitions[key] = {
+            write: [],
+            read: [],
+            survey: []
+        };
+        writeJournalDefinitionsToTextarea(controller.journalDefinitions);
+    }
+    
+    function cancelChanges(controller) {
+        if (!confirm("Are you sure you want to discard recent changes?")) return;
+        writeJournalDefinitionsToTextarea(controller.originalJournalDefinitions);
+        controller.journalDefinitions = readJournalDefinitionsFromTextarea();
+    }
     
     function showJSONChecked(controller, checked) {
         controller.showJSON = checked;
@@ -40,9 +92,13 @@
         jsonForm.style.display = display;
     }
     
-    function readJournalDefinitions() {
+    function writeJournalDefinitionsToTextarea(journalDefinitions) {
+        journalsTextarea.value = JSON.stringify(journalDefinitions, null, 4);
+    }
+    
+    function readJournalDefinitionsFromTextarea() {
         var text = journalsTextarea.value;
-        console.log("readJournalDefinitions", text);
+        console.log("readJournalDefinitionsFromTextarea", text);
         try {
             return JSON.parse(text);
         } catch (e) {
@@ -56,7 +112,6 @@
         jsonForm.style.display = 'none';
         
         journalsTextarea = document.getElementsByName("narrafirma_admin_settings[journals]")[0];
-        readJournalDefinitions();
         
         m.mount(document.getElementById("narrafirma-project-list-editor"), NarraFirmaAdminComponent);
     }
