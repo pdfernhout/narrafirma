07382c8aa500fa34889395699c6ed24eaebf9d42
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-09-25 20:20:34 -0400

    Add stub build.js

---
+++ build.js
@@ -0,0 +1,29 @@
+// TODO: This is not yet finished... Just experimenting....
+
+({
+    baseUrl: "narrafirma/webapp/",
+    paths: {
+        js: "js",
+        mithril: "lib/mithril/mithril",
+        d3: "lib/d3/d3",
+        humane: "lib/humane/humane",
+        lib: "lib"
+    },
+    name: "js/main",
+    out: "main-built.js"
+})
+
+// node ../node_modules/.bin/r.js -o 
+
+// http://requirejs.org/docs/optimization.html#wholeproject
+// https://github.com/requirejs/example-multipage/blob/master/tools/build.js    
+({
+    appDir: "../",
+    baseUrl: "scripts",
+    dir: "../../appdirectory-build",
+    modules: [
+        {
+            name: "main"
+        }
+    ]
+})
