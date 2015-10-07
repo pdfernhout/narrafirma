45b420f7c230c60420d88eff9624c5ae2a3054c2
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-10-07 14:38:17 -0400

    Fix mannWhitneyU so it works (or at least seems to, from limited testing)

--- webapp/source/statistics/mannWhitneyU.ts
+++ webapp/source/statistics/mannWhitneyU.ts
@@ -14,13 +14,19 @@ function mannWhitneyU(x: number[], y: number[]) {
     var n1 = x.length;
     var n2 = y.length;
     
-    var ranked = statisticsCommon.rankdata(x.concat(y));
+    var allValues = x.concat(y);
+    // console.log("mannWhitneyU allValues", allValues);
+    
+    var ranked = statisticsCommon.rankdata(allValues);
+    // console.log("mannWhitneyU ranked", ranked);
     
     // get the x-ranks
     var rankx = ranked.slice(0, n1);    
     
     // the rest are y-ranks
     var ranky = ranked.slice(n1);
+    
+    // console.log("mannWhitneyU rankx, ranky", rankx, ranky);
     
     // calc U for x
     var u1 = n1 * n2 + (n1 * (n1 + 1)) / 2.0 - jStat.sum(rankx);
@@ -31,18 +37,26 @@ function mannWhitneyU(x: number[], y: number[]) {
     var bigu = Math.max(u1, u2);
     var smallu = Math.min(u1, u2);
     
+    // console.log("mannWhitneyU bigu smallu", bigu, smallu);
+    
     // correction factor for tied scores
     var T = Math.sqrt(tiecorrect(ranked));
     if (T === 0) {
         throw new Error("ValueError: All numbers are identical in mannWhitneyU");
     }
+    // console.log("mannWhitneyU T tiecorrect", T);
+    
     var sd = Math.sqrt(T * n1 * n2 * (n1 + n2 + 1) / 12.0);
+    // console.log("mannWhitneyU sd", sd);
     
     // normal approximation for prob calc
     var z = Math.abs((bigu - n1 * n2 / 2.0) / sd);
+    // console.log("mannWhitneyU z", z);
     
-    // TODO: Is the call to jStat.ztest correct, and should it be 1 or 2 sides?
-    return {u: smallu, p: 1.0 - jStat.ztest(z, 1)};
+    var p = 1.0 - jStat.normal.cdf(z, 0, 1);
+    // console.log("mannWhitneyU p", p);
+    
+    return {u: smallu, p: p};
 }
 
 function tiecorrect(rankvals: number[]): number {
@@ -55,7 +69,9 @@ function tiecorrect(rankvals: number[]): number {
     Returns: T correction factor for U or H
     */
     
-    var sorted = rankvals.slice().sort();
+    var sorted = rankvals.slice().sort(function(a, b) {
+        return a - b;
+    });
     
     var n = sorted.length;
     var T = 0.0;
@@ -68,11 +84,32 @@ function tiecorrect(rankvals: number[]): number {
                 i = i + 1;
             }
             T = T + nties * nties * nties - nties;
-        i = i + 1;
         }
+        i = i + 1;
     }
     T = T / (n * n * n - n);
     return 1.0 - T;
 }
+
+function test() {
+    console.log("mannWhitneyU self diagnostic");
+    var result1 = mannWhitneyU([1, 2, 3], [1, 2, 4]);
+    console.log("result1", result1);
+    // Result { u: 4, p: 0.4123703981236436 }
+    
+    // Values from: http://www.stat.purdue.edu/~tqin/system101/method/method_wilcoxon_rank_sum_sas.htm
+    var result2 = mannWhitneyU([17, 20, 170, 315, 22, 190, 64], [22, 29, 13, 16, 15, 18, 15, 6]);
+    console.log("result2", result2);
+    // Result { u: 6.5, p: 0.006380543605407185 }
+    
+    // Values from: http://geographyfieldwork.com/Mann%20Whitney.htm
+    var result3 = mannWhitneyU([7, 3, 6, 2, 4, 3, 5, 5], [3, 5, 6, 4, 6, 5, 7, 5]);
+    console.log("result3", result3);
+    // result3 { u: 23, p: 0.16955853681823607 }
+    
+    // Useful for comparing, but off a bit for p: http://www.socscistatistics.com/tests/mannwhitney/default2.aspx
+}
+
+// test();
 
 export = mannWhitneyU;
