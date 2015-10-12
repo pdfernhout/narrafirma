06bedd2d32ccdd319d4e9105f791d673f88c9a7c
Cynthia Kurtz <cfkurtz@kurtz-fernhout.com>
2015-10-12 16:22:33 -0400

    Removed "introduction" page, moved explanations on it to main (dashboard) help page.

--- webapp/source/applicationPanelSpecifications/administration/page_administration.ts
+++ webapp/source/applicationPanelSpecifications/administration/page_administration.ts
@@ -6,10 +6,17 @@ var panel: Panel = {
     displayName: "Administration",
     panelFields: [
         {
+            id: "administrationVersionLabel",
+            valueType: "none",
+            displayType: "label",
+            // TODO: This is hard coded. Replace with lookup later.
+            displayPrompt: "NarraFirma version: 0.9.0."
+        },
+        {
             id: "administrationIntroLabel",
             valueType: "none",
             displayType: "label",
-            displayPrompt: "In the administration section, you can do house-keeping actions like backing up data. There is also an introduction page for first-time users. Below are links to each activity, along with any reminders you may have entered on them."
+            displayPrompt: "In the administration section, you can do house-keeping actions like backing up data. Below are links to each activity, along with any reminders you may have entered on them."
         }
     ]
 };
--- webapp/source/applicationPanelSpecifications/navigation.ts
+++ webapp/source/applicationPanelSpecifications/navigation.ts
@@ -11,7 +11,6 @@ var sections = [
         sectionName: "Administration",
         pages: [
             "page_administration",
-            "page_introduction",
             "page_importExport"
         ]
     },
