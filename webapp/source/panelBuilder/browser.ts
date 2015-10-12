fadabbba17fd8fb21d1e6ab50302f3600d3eb0de
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-10-12 17:47:47 -0400

    Make help window larger when it is first opened

--- webapp/source/panelBuilder/browser.ts
+++ webapp/source/panelBuilder/browser.ts
@@ -12,12 +12,12 @@ export function launchApplication(url, windowName) {
         }
     }
     if (typeof openedWindowInfo === 'undefined' || openedWindowInfo.window.closed) {
-        var width = Math.round(screen.availWidth / 2);
-        var height = Math.round(screen.availHeight / 3);
-        var windowOpenParams = 'status=1,resizable=1,scrollbars=1,left=200,top=200,width=' + width + ',height=' + height;
+        var width = Math.round(((screen.availWidth || 600) - 100) / 1.3);
+        var height = Math.round(((screen.availHeight || 600) - 150) / 1.1);
+        var windowOpenParams = 'status=1,resizable=1,scrollbars=1,toolbar=yes,left=100,top=150,width=' + width + ',height=' + height;
         var openedWindow = window.open(url, windowName, windowOpenParams);
         // Not sure if moveTo and resizeTo is really needed, since works without them in Firefox
-        openedWindow.moveTo(200, 200);
+        openedWindow.moveTo(100, 150);
         openedWindow.resizeTo(width, height);
         openedWindowInfo = {window: openedWindow, url: url};
         openedWindows[windowName] = openedWindowInfo;
