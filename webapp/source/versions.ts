ec384576aa1b314011fd0fed8acd3d0da63c94a1
Cynthia Kurtz <cfkurtz@kurtz-fernhout.com>
2015-10-21 17:21:56 -0400

    Increased version number to 0.9.3 in preparation for new release.

--- webapp/source/versions.ts
+++ webapp/source/versions.ts
@@ -3,7 +3,7 @@ import kludgeForUseStrict = require("./kludgeForUseStrict");
 
 // Return current versions used by application  
 var versions = {
-    narrafirmaApplication: "0.9.2",
+    narrafirmaApplication: "0.9.3",
     
     questionnaireContentType: "org.workingwithstories.NarraFirmaQuestionnaire",
     questionnaireContentVersion: "0.1.0",
