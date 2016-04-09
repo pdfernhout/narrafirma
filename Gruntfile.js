b8bab628462a64ccf62e323f385f586196b42f34
Benjamin Mosior <bemosior@gmail.com>
2016-04-08 22:12:12 -0400

    Gruntfile.js indentation converted to spaces.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,23 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-shell');
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     shell: {
-    	serve: {
-    		command: function () {
-    			return 'node NarraFirmaServer.js';
-    		},
-    		options: {
-    			execOptions: {
-    				cwd: 'server'
-    			}
-    		}
-    	},
-    	tsc: {
-    		command: function () {
-    			return 'node_modules/ntypescript/bin/tsc';
-    		}
-    	}
+      serve: {
+        command: function () {
+          return 'node NarraFirmaServer.js';
+        },
+        options: {
+          execOptions: {
+            cwd: 'server'
+          }
+        }
+      },
+      tsc: {
+        command: function () {
+          return 'node_modules/ntypescript/bin/tsc';
+        }
+      }
     }
   });
   grunt.registerTask('default', ['shell:tsc','shell:serve']);
