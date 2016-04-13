e78fe1e3980b43f7f017bb06264cbaf77c172c9f
Cynthia Kurtz <cfkurtz@kurtz-fernhout.com>
2016-04-13 13:29:44 -0400

    Improved look of generated catalysis report.
    Added links to "drill down" in catalysis report. Also added styling and
    improved look of report. Also improved colors to match those in "Explore
    patterns" screen.

--- webapp/source/applicationPanelSpecifications/catalysis/panel_addCatalysisReport.ts
+++ webapp/source/applicationPanelSpecifications/catalysis/panel_addCatalysisReport.ts
@@ -10,7 +10,7 @@ var panel: Panel = {
             valueType: "string",
             displayType: "text",
             displayName: "Catalysis report name",
-            displayPrompt: "Please give this catalysis report a short name."
+            displayPrompt: "Please give this catalysis report a short <strong>name</strong>."
         },
         {
             id: "catalysisReport_storyCollections",
@@ -26,14 +26,14 @@ var panel: Panel = {
                 }
             },
             displayName: "Story collections",
-            displayPrompt: "Add one or more story collections to this catalysis report."
+            displayPrompt: "Add one or more <strong>story collections</strong> to this catalysis report."
         },
         {
             id: "catalysisReport_notes",
             valueType: "string",
             displayType: "textarea",
             displayName: "Notes",
-            displayPrompt: "You can enter any extra notes on the catalysis report here."
+            displayPrompt: "You can enter <strong>notes</strong> on the catalysis report here. They will appear at the start of your printed report."
         }
     ]
 };
