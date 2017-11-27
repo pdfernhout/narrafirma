5a8cab618fbaa4b3a28dee0acd494a1206fb1ac0
Cynthia Kurtz <cfkurtz@kurtz-fernhout.com>
2017-11-27 15:27:53 -0500

    Fixed bug in catalysis report printing

--- webapp/source/printing.ts
+++ webapp/source/printing.ts
@@ -730,7 +730,7 @@ export function printCatalysisReport() {
     }
          
     var project = Globals.project();
-    var catalysisReportName = Globals.clientState().catalysisReportIdentifier();
+    var catalysisReportName = Globals.clientState().catalysisReportName();
     if (!catalysisReportName) {
         alert("Please pick a catalysis report to print.");
         return;
