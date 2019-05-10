521a94da84639ed94bec9ca55ca3bd8f674cc388
Cynthia Kurtz <cfkurtz@kurtz-fernhout.com>
2019-05-10 14:24:24 -0400

    Still testing, but preparing version numbers for release

--- webapp/source/versions.ts
+++ webapp/source/versions.ts
@@ -3,7 +3,7 @@ import kludgeForUseStrict = require("./kludgeForUseStrict");
 
 // Return current versions used by application  
 var versions = {
-    narrafirmaApplication: "1.2.6",
+    narrafirmaApplication: "1.3.0",
     
     questionnaireContentType: "org.workingwithstories.NarraFirmaQuestionnaire",
     questionnaireContentVersion: "0.2.0",
