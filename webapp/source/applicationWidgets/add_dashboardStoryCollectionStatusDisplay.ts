d41290fe64067939fe3796ef3ca59c4b2afd28eb
Cynthia Kurtz <cfkurtz@kurtz-fernhout.com>
2022-02-02 16:18:54 -0500

    Added section links below table in case people don't want to click the imagemap buttons

--- webapp/source/applicationWidgets/add_dashboardStoryCollectionStatusDisplay.ts
+++ webapp/source/applicationWidgets/add_dashboardStoryCollectionStatusDisplay.ts
@@ -12,7 +12,7 @@ function mithrilArrayForListOfThingsAndLink(name, list, page) {
     const count = list.length;
     let text = "" + count + " " + name;
     if (count != 1) text += "s";
-    return [m("a.narrafirma-home-page-link", {href: 'javascript:narrafirma_openPage("' + page + '")', title: text}, text)];  
+    return [m("a.narrafirma-home-page-link", {href: 'javascript:narrafirma_openPage("' + page + '")', title: text, tabindex: 0}, text)];  
 }
 
 function add_dashboardStoryCollectionStatusDisplay(panelBuilder: PanelBuilder, model, fieldSpecification): any {
@@ -94,11 +94,11 @@ function add_dashboardStoryCollectionStatusDisplay(panelBuilder: PanelBuilder, m
                 ]),
                 storyCollections.map(function(storyCollection) {
                     const reviewStories = m("a[id=narrafirma-review-stories-url]", 
-                        {href: storyCollection.reviewStoriesURL, title: "Click here to view the stories in this collection."}, storyCollection.shortName);
+                        {href: storyCollection.reviewStoriesURL, title: "Click here to view the stories in this collection.", tabindex: 0}, storyCollection.shortName);
                         const reviewGraphs = m("a[id=narrafirma-review-graphs-url]", 
-                        {href: storyCollection.browseGraphsURL, title: "Click here to review graphs in this collection."}, <any>storyCollection.storyCount);
+                        {href: storyCollection.browseGraphsURL, title: "Click here to review graphs in this collection.", tabindex: 0}, <any>storyCollection.storyCount);
                         const surveyActive = storyCollection.activeOnWeb ? m("a[id=narrafirma-survey-url]", 
-                        {href: storyCollection.surveyURL, target: "_blank", title: "Click here to launch the survey for this collection."}, "yes") : "no";
+                        {href: storyCollection.surveyURL, target: "_blank", title: "Click here to launch the survey for this collection.", tabindex: 0}, "yes") : "no";
                     return m("tr", [
                         m("td", reviewStories),
                         m("td", {style: "text-align: center;"}, reviewGraphs),
@@ -112,9 +112,22 @@ function add_dashboardStoryCollectionStatusDisplay(panelBuilder: PanelBuilder, m
     if (resultItems.length && resultItems[0]) {
         resultItems.unshift(m("p", {style: "margin-top: 0"}, "Quick links for: " + Globals.project().projectNameOrNickname()));
     }
+
+    // section links (to use if can't click on image)
+    const sectionLinks = [
+        m("p"),
+        m("a.narrafirma-home-page-section-link", {href: 'javascript:narrafirma_openPage("page_planning")', title: "Go to planning section", tabindex: 0}, "Planning"),
+        m("a.narrafirma-home-page-section-link", {href: 'javascript:narrafirma_openPage("page_collection")', title: "Go to collection section", tabindex: 0}, "Collection"),
+        m("a.narrafirma-home-page-section-link", {href: 'javascript:narrafirma_openPage("page_catalysis")', title: "Go to catalysis section", tabindex: 0}, "Catalysis"),
+        m("a.narrafirma-home-page-section-link", {href: 'javascript:narrafirma_openPage("page_sensemaking")', title: "Go to sensemaking section", tabindex: 0}, "Sensemaking"),
+        m("a.narrafirma-home-page-section-link", {href: 'javascript:narrafirma_openPage("page_intervention")', title: "Go to intervention section", tabindex: 0}, "Intervention"),
+        m("a.narrafirma-home-page-section-link", {href: 'javascript:narrafirma_openPage("page_return")', title: "Go to return section", tabindex: 0}, "Return"),
+
+    ];
+    resultItems = resultItems.concat(sectionLinks); 
     
     // project admin
-    resultItems = resultItems.concat([m("br"), m("a.narrafirma-home-page-link", {href: 'javascript:narrafirma_openPage("page_administration")', title: "Project administration"}, "Project administration")]); 
+    resultItems = resultItems.concat([m("p"), m("a.narrafirma-home-page-link", {href: 'javascript:narrafirma_openPage("page_administration")', title: "Project administration", tabindex: 0}, "Project administration")]); 
 
     // choose another project
     let chooseProjectLink;
@@ -124,13 +137,14 @@ function add_dashboardStoryCollectionStatusDisplay(panelBuilder: PanelBuilder, m
     } else {
         chooseProjectLink = "../webapp/narrafirma.html";
     }
-    resultItems = resultItems.concat([m("a.narrafirma-home-page-link", {href: chooseProjectLink, title: "Choose another project"}, "Choose another project")]); 
+    resultItems = resultItems.concat([m("a.narrafirma-home-page-link", {href: chooseProjectLink, title: "Choose another project", tabindex: 0}, "Choose another project")]); 
 
     // site admin
     if (Globals.project().currentUserIsSuperUser) {
         // no need to get WordPress URL because superuser account does not exist there
-        resultItems = resultItems.concat([m("a.narrafirma-home-page-link", {href: "\\admin.html", title: "Site administration"}, "Site administration"), m("br")]); 
+        resultItems = resultItems.concat([m("a.narrafirma-home-page-link", {href: "\\admin.html", title: "Site administration", tabindex: 0}, "Site administration"), m("br")]); 
     }
+
     return m("div.narrafirma-dashboard-story-collection-status", resultItems);
 }
 
